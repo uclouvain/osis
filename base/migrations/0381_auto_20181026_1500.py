5efee669371b16b56ed49ff681c6acddc18d27aa
Ndizera Eddy <eddyndizera@gmail.com>
2018-10-30 13:38:54 +0100

    OSIS-2160 Resolved migrations
    	modified:   base/migrations/0380_auto_20181030_1108.py
    	modified:   base/migrations/0381_auto_20181026_1500.py

--- base/migrations/0381_auto_20181026_1500.py
+++ base/migrations/0381_auto_20181026_1500.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0379_auto_20181029_1125'),
+        ('base', '0380_auto_20181030_1108'),
     ]
 
     operations = [
