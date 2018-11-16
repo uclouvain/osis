f2b3cb8688c0da9d29f5960a2d7dc756e38277e5
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-11-16 13:48:54 +0100

    OSIS-2202 indentation

--- base/tests/models/test_prerequisite_item.py
+++ base/tests/models/test_prerequisite_item.py
@@ -76,12 +76,8 @@ class TestPrerequisiteItem(TestCase):
 
     def test_find_by_prerequisite(self):
         self.assertEqual(
-            list(
-                prerequisite_item.find_by_prerequisite(self.prerequisite)
-            ),
-            [
-                self.prerequisite_item
-            ]
+            list(prerequisite_item.find_by_prerequisite(self.prerequisite)),
+            [self.prerequisite_item]
         )
 
 
