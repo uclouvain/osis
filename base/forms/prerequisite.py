e07b0b858dd0058f99939cf588da140b34dc2dba
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-11-20 11:09:27 +0100

    OSIS-2257 Fix form -> accept no prerequisite

--- base/forms/prerequisite.py
+++ base/forms/prerequisite.py
@@ -41,6 +41,7 @@ class LearningUnitPrerequisiteForm(forms.ModelForm):
     prerequisite_string = forms.CharField(
         label=_("Prerequisite"),
         validators=[prerequisite_syntax_validator],
+        required=False,
         help_text=_(
             "<b>Syntax rules</b>:<ul><li>No double parentheses.</li><li>Valid operators are OU or ET.</li><li>The "
             "operator must be the same inside all parentheses (groups).</li><li>The operator that linked groups must "
@@ -67,7 +68,7 @@ class LearningUnitPrerequisiteForm(forms.ModelForm):
         return self.get_grouped_items_from_string(
             prerequisite_string=prerequisite_string,
             main_operator=self.main_operator
-        )
+        ) if prerequisite_string else []
 
     def save(self, commit=True):
         grouped_items = self.cleaned_data['prerequisite_string']
