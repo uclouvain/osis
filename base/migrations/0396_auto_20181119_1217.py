83618384935f9e733d932adbfd0cfca2fc323bb6
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2018-11-20 09:22:44 +0100

    OSIS-2203 Migration

--- base/migrations/0396_auto_20181119_1217.py
+++ base/migrations/0396_auto_20181119_1217.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0394_auto_20181119_1407'),
+        ('base', '0395_auto_20181120_0844'),
     ]
 
     operations = [
