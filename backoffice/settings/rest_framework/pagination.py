698af5b4ca05868a470fde8ca31d9c8ae9ed8b8e
Brugnetti Alessandro <alessandro.brugnetti@uclouvain.be>
2018-11-23 10:24:32 +0100

    Update pagination.py
    Remove comment for max_limit

--- backoffice/settings/rest_framework/pagination.py
+++ backoffice/settings/rest_framework/pagination.py
@@ -27,5 +27,4 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class LimitOffsetPaginationWithUpperBound(LimitOffsetPagination):
-    # Set the maximum limit	value to 100
     max_limit = 100
