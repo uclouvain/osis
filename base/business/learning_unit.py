3cda53193a0da7d5a6dbaa222e7f46c1fdce4207
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-11-29 14:57:16 +0100

    #OSIS-2277 Modification des traductions

--- base/business/learning_unit.py
+++ base/business/learning_unit.py
@@ -51,9 +51,9 @@ from osis_common.utils.datetime import convert_date_to_datetime
 from base.models.enums import entity_container_year_link_type
 # List of key that a user can modify
 
-WORKSHEET_TITLE = 'learning_units'
-XLS_FILENAME = 'learning_units_filename'
-XLS_DESCRIPTION = "List_activities"
+WORKSHEET_TITLE = _('Activities list')
+XLS_FILENAME = _('ActivitiesList')
+XLS_DESCRIPTION = _('Activities list')
 LEARNING_UNIT_TITLES_PART1 = [
     str(_('Code')),
     str(_('Ac yr.')),
