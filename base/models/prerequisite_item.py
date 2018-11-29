f0073061c85250fa5666e2f3bb436b970adc3b07
Christophe Fivet <christophe.fivet@uclouvain.be>
2018-11-29 14:46:53 +0100

    OSIS-2333 Add correction detach method

--- base/models/prerequisite_item.py
+++ base/models/prerequisite_item.py
@@ -72,8 +72,9 @@ def find_by_prerequisite(prerequisite):
     return PrerequisiteItem.objects.filter(prerequisite=prerequisite)
 
 
-def find_by_learning_unit_year_having_prerequisite(learning_unit_year):
-    return PrerequisiteItem.objects.filter(prerequisite__learning_unit_year=learning_unit_year)
+def find_by_learning_unit_year_having_prerequisite(education_group_year, learning_unit_year):
+    return PrerequisiteItem.objects.filter(prerequisite__learning_unit_year=learning_unit_year,
+                                           prerequisite__education_group_year=education_group_year)
 
 
 def find_by_learning_unit_being_prerequisite(learning_unit):
