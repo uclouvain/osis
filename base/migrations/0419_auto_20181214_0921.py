568d37018fce2dcf17402dc5e9d2495b646811a3
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2018-12-17 15:03:06 +0100

    #OSIS-2196 Pull de dev + correction nom de fichier de migration

--- base/migrations/0419_auto_20181214_0921.py
+++ base/migrations/0419_auto_20181214_0921.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0417_auto_20181214_0955'),
+        ('base', '0418_remove_hops_uuid'),
     ]
 
     operations = [
