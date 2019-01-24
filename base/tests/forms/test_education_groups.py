0ca5db68e196b6d8a62d82b592a3f95ae9121b25
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2019-01-24 08:32:47 +0100

    #OSIS-1781 Ajustement du test pour valider l'ordre

--- base/tests/forms/test_education_groups.py
+++ base/tests/forms/test_education_groups.py
@@ -28,6 +28,8 @@ from django.test import TestCase
 
 from base.tests.factories.education_group_type import EducationGroupTypeFactory
 from base.forms.education_groups import EducationGroupFilter
+from django.utils.translation import ugettext_lazy as _
+from base.models.education_group_type import EducationGroupType
 
 
 class TestEducationGroupTypeOrderingForm(TestCase):
@@ -37,13 +39,15 @@ class TestEducationGroupTypeOrderingForm(TestCase):
         self.educ_grp_type_B = EducationGroupTypeFactory(name='B label')
         self.educ_grp_type_A = EducationGroupTypeFactory(name='A label')
 
+        self.form = EducationGroupFilter()
+        self.form.fields["education_group_type"].queryset = EducationGroupType.objects.all().order_by_translated_name()
+
     def test_ordering(self):
-        form = EducationGroupFilter()
-        self.assertCountEqual(list(form.fields["education_group_type"].queryset),
-                            [self.educ_grp_type_A, self.educ_grp_type_B, self.educ_grp_type_D])
+        self.assertEqual(list(self.form.fields["education_group_type"].queryset),
+                         [self.educ_grp_type_A, self.educ_grp_type_B, self.educ_grp_type_D])
         educ_grp_type_C = EducationGroupTypeFactory(name='C label')
-        form = EducationGroupFilter()
-        self.assertCountEqual(
-            list(form.fields["education_group_type"].queryset),
+        self.form.fields["education_group_type"].queryset = EducationGroupType.objects.all().order_by_translated_name()
+        self.assertEqual(
+            list(self.form.fields["education_group_type"].queryset),
             [self.educ_grp_type_A, self.educ_grp_type_B, educ_grp_type_C, self.educ_grp_type_D]
         )
