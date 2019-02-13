6e82c7c767162b84c812ed4dd90e4f367b4516e7
lspitaels <laurent.spitaels@uclouvain.be>
2019-02-13 14:24:31 +0100

    OSIS-2619 show decree, code and name of the domain in he identification page of an education group

--- reference/models/domain.py
+++ reference/models/domain.py
@@ -23,10 +23,11 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.db import models
 from django.core import serializers
-from reference.models.enums import domain_type
+from django.db import models
+
 from osis_common.models.serializable_model import SerializableModel, SerializableModelAdmin
+from reference.models.enums import domain_type
 
 
 class DomainAdmin(SerializableModelAdmin):
@@ -49,7 +50,7 @@ class Domain(SerializableModel):
     national = models.BooleanField(default=False) # True if is Belgian else False
 
     def __str__(self):
-        return self.name
+        return "{}: {} {}".format(self.decree.name, self.code, self.name)
 
 
 def find_all_for_sync():
