7cc240008d62be784a47eec206abd6542b59bc0b
BenJ <benjamin.daubry@uclouvain.be>
2019-03-29 10:55:22 +0100

    OSIS-2779 : Add endpoint for Person roles

--- base/api/views/person.py
+++ base/api/views/person.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2017 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,43 +23,18 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from rest_framework import serializers
+from rest_framework import generics
 
-from base.models.entity_version import EntityVersion
+from base.api.serializers.person import PersonRolesSerializer
 from base.models.person import Person
 
 
-class PersonDetailSerializer(serializers.ModelSerializer):
-    uuid = serializers.UUIDField(required=False)
+class PersonRoles(generics.RetrieveAPIView):
+    """
+       Return the roles of a Person.
+    """
+    name = 'person-detail'
+    queryset = Person.objects.all()
 
-    class Meta:
-        model = Person
-        fields = (
-            'first_name',
-            'last_name',
-            'email',
-            'gender',
-            'uuid'
-        )
-
-
-class PersonRolesSerializer(serializers.ModelSerializer):
-    roles = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Person
-        fields = (
-            'global_id',
-            'roles'
-        )
-
-    def get_roles(self, obj):
-        entities_acronym = set()
-        for person_entity in obj.personentity_set.all():
-            if not person_entity.with_child:
-                acronyms = {person_entity.entity.most_recent_acronymp}
-            else:
-                acronyms = set(row['acronym'] for row in EntityVersion.objects.get_tree(person_entity.entity))
-            entities_acronym |= acronyms
-
-        return entities_acronym
+    serializer_class = PersonRolesSerializer
+    lookup_field = 'global_id'
