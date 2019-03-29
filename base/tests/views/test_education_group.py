f0f931327e1da4a50427399484ff63f0b5d953e9
lspitaels <laurent.spitaels@uclouvain.be>
2019-03-29 08:02:58 +0100

    OSIS-2769 add override_flags in unittest

--- base/tests/views/test_education_group.py
+++ base/tests/views/test_education_group.py
@@ -360,6 +360,7 @@ class EducationGroupPublishViewTestCase(TestCase):
 
 
 @override_flag('education_group_update', active=True)
+@override_flag('education_group_administrative_data_update', active=True)
 class EducationGroupViewTestCase(TestCase):
     def setUp(self):
         today = datetime.date.today()
@@ -550,6 +551,7 @@ class EducationGroupAdministrativedata(TestCase):
 
 
 @override_flag('education_group_update', active=True)
+@override_flag('education_group_administrative_data_update', active=True)
 class EducationGroupEditAdministrativeData(TestCase):
     def setUp(self):
         self.person = PersonFactory()
