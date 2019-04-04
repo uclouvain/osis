a968feaaa8e4a10805c9f05f2fc3236f0b8cd200
Aurélien De Ryck <aurelien.deryck@usaintlouis.be>
2019-04-04 15:54:21 +0200

    OSIS-2789 Remove p in copyright

--- assessments/urls.py
+++ assessments/urls.py
@@ -4,9 +4,9 @@
 #    designed to manage the core business of higher education institutions,
 #    such as universities, faculties, institutes and professional schools.
 #    The core business involves the administration of students, teachers,
-#    courses, programs and so on.p
+#    courses, programs and so on.
 #
-#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
