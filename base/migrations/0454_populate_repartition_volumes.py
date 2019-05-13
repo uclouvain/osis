698111f0e0d93f39c42dea90f418c1f5202f6f43
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2019-05-13 13:47:31 +0200

    fixed migration files

--- base/migrations/0454_populate_repartition_volumes.py
+++ base/migrations/0454_populate_repartition_volumes.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0452_auto_20190510_0935'),
+        ('base', '0453_auto_20190513_0935'),
     ]
 
     operations = [
