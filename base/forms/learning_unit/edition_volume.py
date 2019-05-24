258dff7e2a990331b64d2fde7b416f57b055d81e
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2019-05-24 10:34:50 +0200

    #OSIS-3034 Ajout d'info bulles

--- base/forms/learning_unit/edition_volume.py
+++ base/forms/learning_unit/edition_volume.py
@@ -319,10 +319,12 @@ class SimplifiedVolumeForm(forms.ModelForm):
             'planned_classes'
         )
         widgets = {
-            'hourly_volume_total_annual': forms.TextInput(attrs={'title': _("Volume annuel")}),
-            'hourly_volume_partial_q1': forms.TextInput(attrs={'title': _("Vol. Q1")}),
-            'hourly_volume_partial_q2': forms.TextInput(attrs={'title': _("Vol. Q2")}),
-            'planned_classes': forms.TextInput(attrs={'title': _("Classes")})
+            'hourly_volume_total_annual': forms.TextInput(
+                attrs={'title': _("The annual volume must be equal to the sum of the volumes Q1 and Q2")}
+            ),
+            'hourly_volume_partial_q1': forms.TextInput(attrs={'title': _("Volume Q1")}),
+            'hourly_volume_partial_q2': forms.TextInput(attrs={'title': _("Volume Q2")}),
+            'planned_classes': forms.TextInput(attrs={'title': _("Planned classes")})
         }
 
     def clean(self):
