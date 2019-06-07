f553cb6dcb9099b0df46f0d8c6b14fd735110b71
lspitaels <laurent.spitaels@uclouvain.be>
2019-06-07 15:24:44 +0200

    OSIS-3076 pep8 error

--- base/business/education_groups/create.py
+++ base/business/education_groups/create.py
@@ -180,7 +180,7 @@ def _generate_child_partial_acronym(parent, child_initial_value, child_type):
     if not parent.partial_acronym:
         return ""
     match_result = reg_parent_partial_acronym.search(parent.partial_acronym) or \
-                   reg_common_partial_acronym.search(parent.partial_acronym)
+        reg_common_partial_acronym.search(parent.partial_acronym)
     sigle_ele = match_result.group("sigle_ele")
 
     reg_child_initial_value = re.compile(REGEX_GROUP_PARTIAL_ACRONYM_INITIAL_VALUE)
