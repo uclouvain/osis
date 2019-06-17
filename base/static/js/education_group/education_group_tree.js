4a0f3de8471c5ee774b7500865efdb32110249be
lspitaels <laurent.spitaels@uclouvain.be>
2019-06-17 14:30:10 +0200

    OSIS-3097 Pomme (Ctrl) + click ne fonctionne pas dans l'arbre

--- base/static/js/education_group/education_group_tree.js
+++ base/static/js/education_group/education_group_tree.js
@@ -10,18 +10,19 @@ $(document).ready(function () {
     let $documentTree = $('#panel_file_tree');
 
     $documentTree.bind("state_ready.jstree", function (event, data) {
-
-        // Bind the redirection only when the tree is ready,
-        // however, it reload the page during the loading
-        $documentTree.bind("select_node.jstree", function (event, data) {
-            document.location.href = data.node.a_attr.href;
-        });
-
         // if the tree has never been loaded, execute close_all by default.
         if ($.vakata.storage.get(data.instance.settings.state.key) === null) {
             $(this).jstree('close_all');
         }
+
+        $("a.jstree-anchor").click(function( event ) {
+            let pageLocator = '_self';
+            if (event.ctrlKey) pageLocator = '_blank';
+            window.open($(this).attr('href'), pageLocator);
+        });
     });
+
+
 
     function get_data_from_tree(data) {
         let inst = $.jstree.reference(data.reference),
@@ -33,6 +34,7 @@ $(document).ready(function () {
             element_type: obj.a_attr.element_type,
             has_prerequisite: obj.a_attr.has_prerequisite,
             is_prerequisite: obj.a_attr.is_prerequisite,
+            view_url: obj.a_attr.href,
             attach_url: obj.a_attr.attach_url,
             detach_url: obj.a_attr.detach_url,
             modify_url: obj.a_attr.modify_url
@@ -90,6 +92,14 @@ $(document).ready(function () {
                                     displayInfoMessage(jsonResponse, 'message_info_container')
                                 }
                             });
+                        }
+                    },
+
+                    "open" : {
+                        "label": gettext("Open"),
+                        "action": function (data) {
+                            let __ret = get_data_from_tree(data);
+                            window.open(__ret.view_url, '_blank');
                         }
                     },
 
@@ -197,7 +207,6 @@ $(document).ready(function () {
         }, 250);
     });
 });
-
 
 function toggleNav() {
     let treeVisibility = sessionStorage.getItem("treeVisibility") || "0";
