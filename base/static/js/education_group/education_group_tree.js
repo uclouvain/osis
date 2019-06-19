31d9dc0543ed7a4111c761b7695c24d6749a6dfa
lspitaels <laurent.spitaels@uclouvain.be>
2019-06-19 15:32:30 +0200

    OSIS-3097 debug selection

--- base/static/js/education_group/education_group_tree.js
+++ base/static/js/education_group/education_group_tree.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
     let $documentTree = $('#panel_file_tree');
 
     $documentTree.bind("state_ready.jstree", function (event, data) {
-        data.instance.deselect_all();
         // if the tree has never been loaded, execute close_all by default.
         if ($.vakata.storage.get(data.instance.settings.state.key) === null) {
             $(this).jstree('close_all');
@@ -29,7 +28,6 @@ $(document).ready(function () {
             obj = inst.get_node(data.reference);
 
         return {
-            instance: inst,
             group_element_year_id: obj.a_attr.group_element_year,
             element_id: obj.a_attr.element_id,
             element_type: obj.a_attr.element_type,
@@ -72,7 +70,7 @@ $(document).ready(function () {
                 "selected": false,
             },
             "contextmenu": {
-                "select_node": false,
+                "select_node": true,
                 "items": {
                     "select": {
                         "label": gettext("Select"),
@@ -100,7 +98,6 @@ $(document).ready(function () {
                         "label": gettext("Open"),
                         "action": function (data) {
                             let __ret = get_data_from_tree(data);
-                            __ret.instance.deselect_all();
                             window.open(__ret.view_url, '_blank');
                         }
                     },
