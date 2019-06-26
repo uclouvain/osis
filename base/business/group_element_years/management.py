e28117a49a8f6ae432a1a4b6a5fb78e2b8ebcf79
BenJ <benjamin.daubry@uclouvain.be>
2019-06-26 09:03:44 +0200

    OSIS-3136 : Error message if cannot detach

--- base/business/group_element_years/management.py
+++ base/business/group_element_years/management.py
@@ -109,7 +109,7 @@ def can_link_be_detached(root, link):
     min_reached, max_reached, not_authorized = _check_authorized_relationship(root, link, to_delete=True)
     if link.child_branch.education_group_type.name in min_reached:
         raise AuthorizedRelationshipNotRespectedException(
-            errors=_("The parent must have at least one child of type(s) \"%(types)s\".") % {
+            message=_("The parent must have at least one child of type(s) \"%(types)s\".") % {
                 "types": ', '.join(str(AllTypes.get_value(name)) for name in min_reached)
             }
         )
@@ -120,13 +120,13 @@ def check_authorized_relationship(root, link, to_delete=False):
 
     if link.child_branch.education_group_type.name in min_reached:
         raise AuthorizedRelationshipNotRespectedException(
-            errors=_("The parent must have at least one child of type(s) \"%(types)s\".") % {
+            message=_("The parent must have at least one child of type(s) \"%(types)s\".") % {
                 "types": ', '.join(str(AllTypes.get_value(name)) for name in min_reached)
             }
         )
     elif link.child_branch.education_group_type.name in max_reached:
         raise AuthorizedRelationshipNotRespectedException(
-            errors=_("The number of children of type(s) \"%(child_types)s\" for \"%(parent)s\" "
+            message=_("The number of children of type(s) \"%(child_types)s\" for \"%(parent)s\" "
                      "has already reached the limit.") % {
                        'child_types': ', '.join(str(AllTypes.get_value(name)) for name in max_reached),
                        'parent': root
@@ -134,7 +134,7 @@ def check_authorized_relationship(root, link, to_delete=False):
         )
     elif link.child_branch.education_group_type.name in not_authorized:
         raise AuthorizedRelationshipNotRespectedException(
-            errors=_("You cannot attach \"%(child_types)s\" to \"%(parent)s\" (type \"%(parent_type)s\")") % {
+            message=_("You cannot attach \"%(child_types)s\" to \"%(parent)s\" (type \"%(parent_type)s\")") % {
                 'child_types': ', '.join(str(AllTypes.get_value(name)) for name in not_authorized),
                 'parent': root,
                 'parent_type': AllTypes.get_value(root.education_group_type.name),
