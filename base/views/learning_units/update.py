cc82451a958c87124bc083b4c9a17ab31383caf4
Christophe Fivet <christophe.fivet@uclouvain.be>
2019-07-18 15:35:40 +0200

    OSIS-2303 Fixed bug with components

--- base/views/learning_units/update.py
+++ base/views/learning_units/update.py
@@ -174,7 +174,7 @@ def _save_form_and_display_messages(request, form):
     try:
         records = form.save()
         display_warning_messages(request, getattr(form, 'warnings', []))
-        if len(records) > 1:
+        if 'postponement' in request.POST:
             display_success_messages(request, _('The learning unit has been updated (with report).'))
         else:
             display_success_messages(request, _('The learning unit has been updated (without report).'))
