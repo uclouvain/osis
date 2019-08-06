f8b3963b4b72a7d7db3e97231aaf70f56144290f
BenJ <benjamin.daubry@uclouvain.be>
2019-08-06 09:58:27 +0200

    Fix test post form with formset

--- base/tests/views/education_groups/test_update.py
+++ base/tests/views/education_groups/test_update.py
@@ -308,6 +308,8 @@ class TestUpdate(TestCase):
             "start_year": 2010,
             "constraint_type": "",
             "diploma_printing_title": "Diploma Title",
+            'form-TOTAL_FORMS': 0,
+            'form-INITIAL_FORMS': 0,
         }
         response = self.client.post(self.training_url, data=data)
         self.assertEqual(response.status_code, 302)
@@ -387,6 +389,8 @@ class TestUpdate(TestCase):
             "end_year": 2018,
             "constraint_type": "",
             "diploma_printing_title": "Diploma Title",
+            'form-TOTAL_FORMS': 0,
+            'form-INITIAL_FORMS': 0,
         }
         response = self.client.post(self.training_url, data=data)
         messages = [m.message for m in get_messages(response.wsgi_request)]
