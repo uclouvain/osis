ded267e5d5285dc334e76559d9edfb7d6c046f98
Ndizera Eddy <eddyndizera@gmail.com>
2019-08-26 14:31:03 +0200

    [OSIS-3357] style excel
    	modified:   base/business/education_groups/reporting.py

--- base/business/education_groups/reporting.py
+++ base/business/education_groups/reporting.py
@@ -28,9 +28,14 @@ import itertools
 from django.db.models import QuerySet
 from django.utils.translation import gettext as _
 from openpyxl import Workbook
+from openpyxl.styles import Style, Border, Side, Color, PatternFill, Font
+from openpyxl.styles.borders import BORDER_THICK
+from openpyxl.styles.colors import RED
 
 from base.models.education_group_year import EducationGroupYear
-from osis_common.document.xls_build import _build_worksheet, CONTENT_KEY, HEADER_TITLES_KEY, WORKSHEET_TITLE_KEY
+from base.models.enums.prerequisite_operator import AND, OR
+from osis_common.document.xls_build import _build_worksheet, CONTENT_KEY, HEADER_TITLES_KEY, WORKSHEET_TITLE_KEY, \
+    STYLED_CELLS, STYLE_NO_GRAY
 
 
 def generate_prerequisites_workbook(egy: EducationGroupYear, prerequisites_qs: QuerySet):
@@ -40,9 +45,13 @@ def generate_prerequisites_workbook(egy: EducationGroupYear, prerequisites_qs: Q
         WORKSHEET_TITLE_KEY: _("Formation prerequisites"),
         HEADER_TITLES_KEY: _build_header(egy),
         CONTENT_KEY: _build_content(prerequisites_qs),
+        STYLED_CELLS: _style_cells(prerequisites_qs)
     }
 
     _build_worksheet(worksheet_data, workbook, 0)
+
+    _merge_cells(prerequisites_qs, workbook)
+    _post_style_cell(workbook)
 
     return workbook
 
@@ -101,3 +110,81 @@ def _build_content(prerequisites_qs: QuerySet):
                     ]
                 )
     return content
+
+
+def _style_cells(prerequisites_qs: QuerySet):
+    STYLE_BORDER_BOTTOM = Style(
+        border=Border(
+            bottom=Side(
+                border_style=BORDER_THICK, color=Color('FF000000')
+            )
+        )
+    )
+    STYLE_GRAY = Style(fill=PatternFill(patternType='solid', fgColor=Color('D1D1D1')))
+    STYLE_LIGHT_GRAY = Style(fill=PatternFill(patternType='solid', fgColor=Color('E1E1E1')))
+    STYLE_LIGHTER_GRAY = Style(fill=PatternFill(patternType='solid', fgColor=Color('F1F1F1')))
+
+    luy_acronym_cells = []
+    row_index = 3
+    for prerequisite in prerequisites_qs:
+        luy_acronym_cells.append(
+            "A{row_index}".format(row_index=row_index)
+        )
+        row_index += (len(prerequisite.items) + 1)
+
+    luy_title_cells = []
+    row_index = 3
+    for prerequisite in prerequisites_qs:
+        luy_title_cells.append(
+            "B{row_index}".format(row_index=row_index)
+        )
+        row_index += (len(prerequisite.items) + 1)
+
+    items_cells = []
+    row_index = 3
+    for prerequisite in prerequisites_qs:
+        for i, item in enumerate(prerequisite.items):
+            if i % 2 == 0:
+                items_cells.append(
+                    "C{row_index}".format(row_index=row_index + i + 1)
+                )
+                items_cells.append(
+                    "D{row_index}".format(row_index=row_index + i + 1)
+                )
+        row_index += len(prerequisite.items) + 1
+
+    return {
+        STYLE_NO_GRAY: ["A1", "B1"],
+        STYLE_BORDER_BOTTOM: ["A2"],
+        STYLE_GRAY: luy_acronym_cells,
+        STYLE_LIGHT_GRAY: luy_title_cells,
+        STYLE_LIGHTER_GRAY: items_cells
+    }
+
+
+def _merge_cells(prerequisites_qs, workbook):
+    worksheet = workbook.worksheets[0]
+    row_index = 3
+    for prerequisite in prerequisites_qs:
+        worksheet.merge_cells(start_row=row_index, end_row=row_index, start_column=2, end_column=4)
+        row_index += (len(prerequisite.items) + 1)
+
+
+def _post_style_cell(workbook: Workbook):
+    worksheet = workbook.worksheets[0]
+    last_operator = None
+
+    main_operator = None
+    for row_index in range(3, worksheet.max_row + 1):
+        cell = worksheet.cell(row=row_index, column=2)
+
+        if not cell.value or cell.value not in (_(AND), _(OR)):
+            main_operator = None
+
+        elif main_operator is None:
+            main_operator = cell.value
+
+        elif main_operator != cell.value:
+            cell.style = Style(
+                font=Font(color=RED)
+            )
