0174099f747c89889ae865fd0a85144814bf224d
lspitaels <laurent.spitaels@uclouvain.be>
2019-08-29 09:21:36 +0200

    OSIS-3434 debug when entity is not academic

--- base/business/education_groups/group_element_year_tree.py
+++ base/business/education_groups/group_element_year_tree.py
@@ -35,7 +35,7 @@ from base.models.education_group_year import EducationGroupYear
 from base.models.entity_version import build_current_entity_version_structure_in_memory, EntityVersion, \
     get_entity_version_parent_or_itself_from_type, get_structure_of_entity_version
 from base.models.enums.education_group_types import MiniTrainingType, GroupType
-from base.models.enums.entity_type import SECTOR, FACULTY
+from base.models.enums.entity_type import SECTOR, FACULTY, SCHOOL, DOCTORAL_COMMISSION
 from base.models.enums.link_type import LinkTypes
 from base.models.enums.proposal_type import ProposalType
 from base.models.group_element_year import GroupElementYear, fetch_all_group_elements_in_tree
@@ -272,7 +272,7 @@ class EducationGroupHierarchy:
             if self.learning_unit_year.externallearningunityear.mobility:
                 return False
         try:
-            if self.cache_entity_parent_root.entity_type not in [SECTOR, FACULTY]:
+            if self.cache_entity_parent_root.entity_type in [SCHOOL, DOCTORAL_COMMISSION]:
                 root = get_entity_version_parent_or_itself_from_type(
                     self.cache_structure,
                     self.cache_entity_parent_root.acronym,
