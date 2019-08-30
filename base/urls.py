dc26a245af088abd5e4808a0df4bbfd69d340530
Christophe Fivet <christophe.fivet@uclouvain.be>
2019-08-30 14:08:58 +0200

    OSIS-3443 Remove not use AutocompleteAcademicYear

--- base/urls.py
+++ base/urls.py
@@ -42,7 +42,6 @@ from base.views import learning_achievement, search, education_groups, user_list
 from base.views import learning_unit, offer, common, institution, organization, academic_calendar, \
     my_osis, entity, student, notifications
 from base.views import teaching_material
-from base.views.academic_year import AcademicYearAutocomplete, AcademicYearAutocompleteLimited
 from base.views.filter import filter_cities_by_country, filter_campus_by_city
 from base.views.learning_units.attribution import DeleteAttribution, EditAttributionView, AddAttribution
 from base.views.learning_units.charge_repartition import AddChargeRepartition, \
