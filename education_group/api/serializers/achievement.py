41dc84a03badaccaed28dd5bea9d6c15275a7287
BenJ <benjamin.daubry@uclouvain.be>
2019-09-13 14:29:13 +0200

    OSIS-3522 : GeneralInformation & Achievement Serializers

--- education_group/api/serializers/achievement.py
+++ education_group/api/serializers/achievement.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,18 +23,41 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.conf.urls import url
+from rest_framework import serializers
 
-from education_group.api.views.general_information import GeneralInformation
-from education_group.api.views.training import TrainingList, TrainingDetail
 
-app_name = "education_group"
-urlpatterns = [
-    url(r'^trainings/$', TrainingList.as_view(), name=TrainingList.name),
-    url(r'^trainings/(?P<uuid>[0-9a-f-]+)$', TrainingDetail.as_view(), name=TrainingDetail.name),
-    url(
-        r'^trainings/(?P<year>[\d]{4})/(?P<language>[\w]{2})/(?P<acronym>[\w]+)$',
-        GeneralInformation.as_view(),
-        name=GeneralInformation.name
-    )
-]
+class DetailedAchievementSerializer(serializers.Serializer):
+    code_name = serializers.CharField(read_only=True)
+    text = serializers.CharField(read_only=True)
+
+    class Meta:
+        fields = (
+            'code_name',
+            'text'
+        )
+
+
+class AchievementSerializer(serializers.Serializer):
+    teaser = serializers.CharField(read_only=True)
+    detailed_achievements = DetailedAchievementSerializer(read_only=True, many=True)
+    code_name = serializers.CharField(read_only=True)
+
+    class Meta:
+        fields = (
+            'teaser',
+            'detailed_achievements',
+            'code_name'
+        )
+
+
+class AchievementsSerializer(serializers.Serializer):
+    intro = serializers.CharField(read_only=True)
+    blocs = AchievementSerializer(many=True, read_only=True)
+    extra = serializers.CharField(read_only=True)
+
+    class Meta:
+        fields = (
+            'intro',
+            'blocs',
+            'extra'
+        )
---
+++ education_group/api/serializers/section.py
@@ -0,0 +1,70 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2018 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from rest_framework import serializers
+
+from education_group.api.serializers.achievement import AchievementsSerializer
+from webservices import business
+
+
+class SectionSerializer(serializers.Serializer):
+    id = serializers.CharField(source='label', read_only=True)
+    label = serializers.CharField(source='translated_label', read_only=True)
+    content = serializers.CharField(source='text', read_only=True)
+    free_text = serializers.CharField(read_only=True, required=False)
+
+    class Meta:
+        fields = (
+            'id',
+            'label',
+            'content',
+            'free_text'
+        )
+
+
+class SectionAchievementSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    label = serializers.CharField(source='id', read_only=True)
+    content = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        fields = (
+            'id',
+            'label',
+            'content',
+        )
+
+    def get_content(self, obj):
+        egy = self.context.get('egy')
+        language = self.context.get('lang')
+        intro_extra_content = business.get_intro_extra_content_achievements(egy, language)
+        return AchievementsSerializer(
+            {
+                'intro': intro_extra_content.get('skills_and_achievements_introduction') or None,
+                'blocs': business.get_achievements(egy, language),
+                'extra': intro_extra_content.get('skills_and_achievements_additional_text') or None
+            },
+            context=self.context
+        ).data
