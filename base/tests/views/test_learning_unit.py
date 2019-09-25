125646a1f2c5e77eb10a00e29ca4de6494da92d8
Ndizera Eddy <eddyndizera@gmail.com>
2019-09-25 16:15:48 +0200

    [OSIS-3527] replace LearningUnitYearForm with filterset in test
    	modified:   base/forms/learning_unit/search_form.py
    	modified:   base/tests/views/test_learning_unit.py

--- base/tests/views/test_learning_unit.py
+++ base/tests/views/test_learning_unit.py
@@ -51,7 +51,7 @@ from base.enums.component_detail import VOLUME_TOTAL, VOLUME_Q1, VOLUME_Q2, PLAN
     VOLUME_REQUIREMENT_ENTITY, VOLUME_ADDITIONAL_REQUIREMENT_ENTITY_1, VOLUME_ADDITIONAL_REQUIREMENT_ENTITY_2, \
     VOLUME_TOTAL_REQUIREMENT_ENTITIES, REAL_CLASSES
 from base.forms.learning_unit.learning_unit_create import LearningUnitModelForm
-from base.forms.learning_unit.search_form import LearningUnitYearForm
+from base.forms.learning_unit.search_form import LearningUnitFilter
 from base.forms.learning_unit_specifications import LearningUnitSpecificationsForm, LearningUnitSpecificationsEditForm
 from base.models.academic_year import AcademicYear
 from base.models.enums import active_status, education_group_categories, \
@@ -98,7 +98,6 @@ from base.views.learning_unit import learning_unit_specifications_edit
 from base.views.learning_units.create import create_partim_form
 from base.views.learning_units.detail import SEARCH_URL_PART
 from base.views.learning_units.pedagogy.read import learning_unit_pedagogy
-from base.views.learning_units.search import learning_units_service_course
 from cms.enums import entity_name
 from cms.tests.factories.text_label import TextLabelFactory
 from cms.tests.factories.translated_text import TranslatedTextFactory
@@ -548,7 +547,7 @@ class LearningUnitViewTestCase(TestCase):
 
     def service_course_search(self, filter_data, number_of_results):
         self._prepare_context_learning_units_search()
-        response = self.client.get(reverse(learning_units_service_course), data=filter_data)
+        response = self.client.get(reverse("learning_units_service_course"), data=filter_data)
 
         self.assertTemplateUsed(response, 'learning_units.html')
         self.assertEqual(response.context['learning_units_count'], number_of_results)
@@ -1154,9 +1153,9 @@ class TestCreateXls(TestCase):
 
     @mock.patch("osis_common.document.xls_build.generate_xls")
     def test_generate_xls_data_with_a_learning_unit(self, mock_generate_xls):
-        a_form = LearningUnitYearForm({"acronym": self.learning_unit_year.acronym}, service_course_search=False)
-        self.assertTrue(a_form.is_valid())
-        found_learning_units = a_form.get_activity_learning_units()
+        learning_unit_filter = LearningUnitFilter({"acronym": self.learning_unit_year.acronym})
+        self.assertTrue(learning_unit_filter.is_valid())
+        found_learning_units = learning_unit_filter.qs
         create_xls(self.user, found_learning_units, None)
         xls_data = [[self.learning_unit_year.academic_year.name, self.learning_unit_year.acronym,
                      self.learning_unit_year.complete_title,
@@ -1473,9 +1472,12 @@ class TestLearningUnitProposalComparison(TestCase):
                  "hourly_volume_partial_q1": self.learning_component_year_practical.hourly_volume_partial_q1,
                  "hourly_volume_partial_q2": self.learning_component_year_practical.hourly_volume_partial_q2,
                  "hourly_volume_total_annual": self.learning_component_year_practical.hourly_volume_total_annual,
-                 "repartition_volume_requirement_entity": self.learning_component_year_practical.repartition_volume_requirement_entity,
-                 "repartition_volume_additional_entity_1": self.learning_component_year_practical.repartition_volume_additional_entity_1,
-                 "repartition_volume_additional_entity_2": self.learning_component_year_practical.repartition_volume_additional_entity_2
+                 "repartition_volume_requirement_entity": self.learning_component_year_practical.
+                     repartition_volume_requirement_entity,
+                 "repartition_volume_additional_entity_1": self.learning_component_year_practical.
+                     repartition_volume_additional_entity_1,
+                 "repartition_volume_additional_entity_2": self.learning_component_year_practical.
+                     repartition_volume_additional_entity_2
                  }
             ],
             "volumes": {
