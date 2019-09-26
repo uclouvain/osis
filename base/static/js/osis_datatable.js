1df459cfd9079de4d7a795b1a59d7c08cd076536
Ndizera Eddy <eddyndizera@gmail.com>
2019-09-26 10:02:14 +0200

    [OSIS-3527] move search datable js in common file
    	new file:   base/static/js/osis_datatable.js
    	modified:   base/templates/learning_unit/search/base.html
    	modified:   base/templates/learning_unit/search/description_fiche.html
    	modified:   base/templates/learning_unit/search/external.html
    	modified:   base/templates/learning_unit/search/proposal.html

---
+++ base/static/js/osis_datatable.js
@@ -0,0 +1,41 @@
+function initializeDataTable(tableId, storageKey, pageNumber, itemsPerPage, ajaxUrl, columnDefs){
+    setEventKeepIds(tableId, storageKey);
+    let domTable = $('#' + tableId);
+    domTable.DataTable(
+    {
+        columnDefs: columnDefs,
+        "stateSave": true,
+        "paging" : false,
+        "ordering" : true,
+        "orderMulti": false,
+        "serverSide": true,
+        "ajax" : {
+            "url": ajaxUrl,
+            "type": "GET",
+            "dataSrc": "object_list",
+            "data": function (d){
+                let querystring = getDataAjaxTable(domTable, d, pageNumber);
+                querystring["paginator_size"] = itemsPerPage;
+                return querystring;
+            },
+            "traditional": true
+        },
+        "info"  : false,
+        "searching" : false,
+        "language": {
+            "oAria": {
+                "sSortAscending":  "{% trans 'activate to sort column ascending'%}",
+                "sSortDescending": "{% trans 'activate to sort column descending'%}"
+            }
+        }
+    });
+}
+
+function prepare_xls(e, action_value){
+    e.preventDefault();
+    var status = $("#xls_status");
+    status.val(action_value);
+    $("#download_xls").submit();
+    status.val('');
+}
+
