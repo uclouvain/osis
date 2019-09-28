fe627c96b25563add61d9a3eb0fa8787d15da604
Ndizera Eddy <eddyndizera@gmail.com>
2019-09-28 10:17:47 +0200

    [django2] fix test with upgrade of django autocomplete
    	modified:   base/tests/views/education_groups/test_search.py
    	modified:   base/tests/views/test_organization.py

--- base/tests/views/test_organization.py
+++ base/tests/views/test_organization.py
@@ -162,7 +162,7 @@ class TestCountryAutocomplete(TestCase):
         self.assertEqual(response.status_code, 200)
         results = _get_results_from_autocomplete_response(response)
 
-        expected_results = [{'text': self.country.name, 'id': str(self.country.pk)}]
+        expected_results = [{'text': self.country.name, 'selected_text': self.country.name, 'id': str(self.country.pk)}]
 
         self.assertListEqual(results, expected_results)
 
@@ -185,6 +185,7 @@ class TestCampusAutocomplete(TestCase):
         response = self.client.get(self.url, data={'q': 'univ'})
 
         expected_results = [{'text': "{} ({})".format(self.organization.name, self.campus.name),
+                             'selected_text': "{} ({})".format(self.organization.name, self.campus.name),
                              'id': str(self.campus.pk)}]
 
         self.assertEqual(response.status_code, 200)
@@ -206,6 +207,7 @@ class TestCampusAutocomplete(TestCase):
             data={'forward': '{"country_external_institution": "%s"}' % self.organization_address.country.pk}
         )
         expected_results = [{'text': "{} ({})".format(self.organization.name, self.campus.name),
+                             'selected_text': "{} ({})".format(self.organization.name, self.campus.name),
                              'id': str(self.campus.pk)}]
         self.assertEqual(response.status_code, 200)
         results = _get_results_from_autocomplete_response(response)
