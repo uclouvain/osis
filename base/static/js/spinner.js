d38f0911369a7b6a0a940f5b1275d5a488d4e020
lspitaels <laurent.spitaels@uclouvain.be>
2019-10-01 09:13:45 +0200

    OSIS-3397 update to support ESC key

--- base/static/js/spinner.js
+++ base/static/js/spinner.js
@@ -1,9 +1,20 @@
 $( document ).ready(function() {
-    $("#loader").hide();
-    document.getElementById("overlay").style.display = "none";
+    closeOverlaySpinner();
+});
+
+$( document ).on( 'keyup', function ( e ) {
+    if ( e.key === 'Escape' ) { // ESC
+        closeOverlaySpinner();
+    }
 });
 
 window.addEventListener('beforeunload', function (e) {
   $("#loader").show();
   document.getElementById("overlay_fadein").style.display = "block";
 });
+
+function closeOverlaySpinner(){
+    $("#loader").hide();
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay_fadein").style.display = "none";
+}
