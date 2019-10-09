e8ed5df0d87ecd2ca85e718e1fa895fbec6672db
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2019-10-09 11:33:45 +0200

    OSIS-3602 Correct PEP8 on form/group_element_year in program_management app

--- program_management/forms/group_element_year.py
+++ program_management/forms/group_element_year.py
@@ -26,7 +26,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from program_management.business.group_element_years.attach import AttachEducationGroupYearStrategy, AttachLearningUnitYearStrategy
+from program_management.business.group_element_years.attach import AttachEducationGroupYearStrategy, \
+    AttachLearningUnitYearStrategy
 from program_management.business.group_element_years.management import CheckAuthorizedRelationshipAttach
 from base.models.enums import education_group_categories
 from base.models.group_element_year import GroupElementYear
