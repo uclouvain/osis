9439fe86e8c7255f2d00f585c936566a241a5ee8
Ndizera Eddy <eddyndizera@gmail.com>
2019-10-18 16:44:11 +0200

    [OSIS-3540] change form parameters on quick search
    	modified:   base/templates/base/quick_search_inner.html
    	modified:   base/views/quick_search.py

--- base/views/quick_search.py
+++ base/views/quick_search.py
@@ -21,19 +21,92 @@
 #  at the root of the source code of this program.  If not,
 #  see http://www.gnu.org/licenses/.
 # ############################################################################
-from abc import ABC
 
 from django import forms
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.db.models import Q
 from django.http import Http404
-from django.views.generic import ListView
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
+from django_filters import FilterSet, filters
+from django_filters.views import FilterView
 
-from base.models.academic_year import starting_academic_year, AcademicYear
+from base.models.academic_year import AcademicYear
 from base.models.education_group_year import EducationGroupYear
-from base.models.learning_unit_year import LearningUnitYear
+from base.models.learning_unit_year import LearningUnitYear, LearningUnitYearQuerySet
 from base.utils.cache import CacheFilterMixin
 from base.views.mixins import AjaxTemplateMixin
+
+
+class QuickEducationGroupYearFilter(FilterSet):
+    acronym = filters.CharFilter(
+        field_name="acronym",
+        lookup_expr='icontains',
+        max_length=40,
+        required=False,
+        label=_('Acronym/Short title'),
+    )
+    title = filters.CharFilter(
+        field_name="title",
+        lookup_expr='icontains',
+        max_length=255,
+        required=False,
+        label=_('Title')
+    )
+
+    class Meta:
+        model = EducationGroupYear
+        fields = [
+            'acronym',
+            'title',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.data:
+            self.queryset = EducationGroupYear.objects.none()
+
+
+class QuickLearningUnitYearFilter(FilterSet):
+    academic_year = filters.ModelChoiceFilter(
+        queryset=AcademicYear.objects.all(),
+        required=False,
+        label=_('Ac yr.'),
+        empty_label=pgettext_lazy("plural", "All"),
+    )
+    acronym = filters.CharFilter(
+        field_name="acronym",
+        lookup_expr="iregex",
+        max_length=40,
+        required=False,
+        label=_('Code'),
+    )
+    title = filters.CharFilter(
+        field_name="full_title",
+        lookup_expr="icontains",
+        max_length=40,
+        label=_('Title'),
+    )
+
+    class Meta:
+        model = LearningUnitYear
+        fields = [
+            "academic_year",
+            "acronym",
+            "title",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queryset = self.get_queryset()
+
+    def get_queryset(self):
+        # Need this close so as to return empty query by default when form is unbound
+        if not self.data:
+            return LearningUnitYear.objects.none()
+        queryset = LearningUnitYear.objects_with_container.select_related(
+            'academic_year',
+        )
+        queryset = LearningUnitYearQuerySet.annotate_full_title_class_method(queryset)
+        return queryset
 
 
 class QuickSearchForm(forms.Form):
@@ -41,26 +114,13 @@ class QuickSearchForm(forms.Form):
     search_text = forms.CharField(required=False)
 
 
-class QuickSearchGenericView(PermissionRequiredMixin, CacheFilterMixin, AjaxTemplateMixin, ListView, ABC):
+# FIXME List should be empty
+class QuickSearchGenericView(PermissionRequiredMixin, CacheFilterMixin, AjaxTemplateMixin, FilterView):
     """ Quick search generic view for learning units and education group year """
     paginate_by = "12"
     ordering = 'academic_year', 'acronym',
     template_name = 'base/quick_search_inner.html'
     cache_exclude_params = 'page',
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        if self.request.GET.get('academic_year'):
-            qs = qs.filter(academic_year=self.request.GET.get('academic_year'))
-
-        search_text = self.request.GET.get('search_text')
-        if search_text:
-            qs = self.search_text_filter(qs, search_text)
-
-        return qs
-
-    def search_text_filter(self, qs, search_text):
-        raise NotImplementedError
 
     def paginate_queryset(self, queryset, page_size):
         """ The cache can store a wrong page number,
@@ -75,19 +135,24 @@ class QuickSearchGenericView(PermissionRequiredMixin, CacheFilterMixin, AjaxTemp
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        academic_yr = AcademicYear.objects.get(pk=self.request.GET.get('academic_year')) \
-            if self.request.GET.get('academic_year') else starting_academic_year()
-
-        context['form'] = QuickSearchForm(
-            initial={
-                'academic_year': academic_yr,
-                'search_text': self.request.GET.get('search_text', ''),
-            }
-        )
-
         # Save in session the last model used in the quick search.
         self.request.session['quick_search_model'] = self.model.__name__
+        return context
+
+
+class QuickSearchEducationGroupYearView(QuickSearchGenericView):
+    model = EducationGroupYear
+    permission_required = 'base.can_access_education_group'
+
+    filterset_class = QuickEducationGroupYearFilter
+    context_object_name = 'eg_object_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['eg_form'] = context["filter"].form
+        context['lu_form'] = QuickLearningUnitYearFilter().form
+
         return context
 
 
@@ -95,20 +160,13 @@ class QuickSearchLearningUnitYearView(QuickSearchGenericView):
     model = LearningUnitYear
     permission_required = 'base.can_access_learningunit'
 
-    def search_text_filter(self, qs, search_text):
-        return qs.filter(
-            Q(acronym__icontains=search_text) | Q(specific_title__icontains=search_text)
-        ).select_related('learning_container_year')
+    filterset_class = QuickLearningUnitYearFilter
+    context_object_name = 'lu_object_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-class QuickSearchEducationGroupYearView(QuickSearchGenericView):
-    model = EducationGroupYear
-    permission_required = 'base.can_access_education_group'
+        context['lu_form'] = context["filter"].form
+        context['eg_form'] = QuickEducationGroupYearFilter().form
 
-    def search_text_filter(self, qs, search_text):
-        return qs.filter(
-            Q(acronym__icontains=search_text) |
-            Q(title__icontains=search_text) |
-            Q(title_english__icontains=search_text) |
-            Q(partial_acronym__icontains=search_text)
-        )
+        return context
