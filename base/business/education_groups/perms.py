703cf0df59aa62ca1cbf68457b98f99346fe08f8
lspitaels <laurent.spitaels@uclouvain.be>
2019-10-23 14:10:10 +0200

    OSIS-3413 add unittest for _is_eligible_certificate_aims + correct test in _is_eligible_certificate_aims

--- base/business/education_groups/perms.py
+++ base/business/education_groups/perms.py
@@ -150,9 +150,9 @@ def _is_eligible_education_group(person, education_group, raise_exception):
 
 def _is_eligible_certificate_aims(person, education_group, raise_exception):
     if education_group.education_group_type.category != TRAINING:
-        result = False
-        msg = _("This education group is not editable during this period.")
-        can_raise_exception(raise_exception, result, msg)
+        if raise_exception:
+            raise PermissionDenied(_("This education group is not editable during this period.").capitalize())
+        return False
 
     return check_link_to_management_entity(education_group, person, raise_exception)
 
