5aa271ed68e822c91b30805a9ecb7add4dca676b
BenJ <benjamin.daubry@uclouvain.be>
2019-11-04 10:24:11 +0100

    OSIS-3656 : Change var to const/let

--- base/static/js/education_group/update_training_errors.js
+++ base/static/js/education_group/update_training_errors.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    var tabs = ['identification', 'content', 'diploma'];
+    const tabs = ['identification', 'content', 'diploma'];
     tabs.forEach(function (tab_name) {
-        var tab = document.getElementById('tab_' + tab_name);
-        var spn_tab_errors = $("#spn_" + tab_name + "_errors");
+        const tab = document.getElementById('tab_' + tab_name);
+        let spn_tab_errors = $("#spn_" + tab_name + "_errors");
         spn_tab_errors.empty();
         if (tab && tab.getElementsByClassName('has-error').length > 0) {
             spn_tab_errors.empty();
