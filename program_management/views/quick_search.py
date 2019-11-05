fdce22477a19eb247a69cb5ddb4824992e0de197
Ndizera Eddy <eddyndizera@gmail.com>
2019-11-05 19:59:00 +0100

    [OSIS-3654] add tests on quick search views
    	new file:   base/forms/education_group/search/__init__.py
    	new file:   base/forms/education_group/search/quick_search.py
    	new file:   base/forms/learning_unit/search/quick_search.py
    	deleted:    base/tests/views/test_quicksearch.py
    	new file:   program_management/tests/views/test_quicksearch.py
    	modified:   program_management/views/quick_search.py

--- program_management/views/quick_search.py
+++ program_management/views/quick_search.py
@@ -21,134 +21,19 @@
 #  at the root of the source code of this program.  If not,
 #  see http://www.gnu.org/licenses/.
 # ############################################################################
-from django import forms
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
-from django.utils.translation import gettext_lazy as _, pgettext_lazy
-from django_filters import FilterSet, filters, OrderingFilter
 from django_filters.views import FilterView
 
-from base.models.academic_year import AcademicYear
+from base.forms.education_group.search.quick_search import QuickEducationGroupYearFilter
+from base.forms.learning_unit.search.quick_search import QuickLearningUnitYearFilter
 from base.models.education_group_year import EducationGroupYear
-from base.models.enums import education_group_categories
-from base.models.learning_unit_year import LearningUnitYear, LearningUnitYearQuerySet
+from base.models.learning_unit_year import LearningUnitYear
 from base.utils.cache import CacheFilterMixin
 from base.utils.search import SearchMixin
 from base.views.mixins import AjaxTemplateMixin
 from education_group.api.serializers.education_group import EducationGroupSerializer
 from learning_unit.api.serializers.learning_unit import LearningUnitSerializer
-
-
-class QuickEducationGroupYearFilter(FilterSet):
-    academic_year = filters.ModelChoiceFilter(
-        queryset=AcademicYear.objects.all(),
-        required=False,
-        label=_('Ac yr.'),
-        empty_label=pgettext_lazy("plural", "All"),
-    )
-    acronym = filters.CharFilter(
-        field_name="acronym",
-        lookup_expr='icontains',
-        max_length=40,
-        required=False,
-        label=_('Acronym/Short title'),
-    )
-    title = filters.CharFilter(
-        field_name="title",
-        lookup_expr='icontains',
-        max_length=255,
-        required=False,
-        label=_('Title')
-    )
-
-    ordering = OrderingFilter(
-        fields=(
-            ('acronym', 'acronym'),
-            ('title', 'title'),
-        ),
-        widget=forms.HiddenInput
-    )
-
-    class Meta:
-        model = EducationGroupYear
-        fields = [
-            'acronym',
-            'title',
-            'academic_year'
-        ]
-
-    def __init__(self, *args, initial=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.queryset = self.get_queryset()
-        if initial:
-            self.form.fields["academic_year"].initial = initial["academic_year"]
-
-    def get_queryset(self):
-        # Need this close so as to return empty query by default when form is unbound
-        if not self.data:
-            return EducationGroupYear.objects.none()
-        queryset = EducationGroupYear.objects.filter(
-            education_group_type__category__in=[
-                education_group_categories.Categories.GROUP.name,
-                education_group_categories.Categories.MINI_TRAINING.name,
-            ]
-        )
-        return queryset
-
-
-class QuickLearningUnitYearFilter(FilterSet):
-    academic_year = filters.ModelChoiceFilter(
-        queryset=AcademicYear.objects.all(),
-        required=False,
-        label=_('Ac yr.'),
-        empty_label=pgettext_lazy("plural", "All"),
-    )
-    acronym = filters.CharFilter(
-        field_name="acronym",
-        lookup_expr="iregex",
-        max_length=40,
-        required=False,
-        label=_('Code'),
-    )
-    title = filters.CharFilter(
-        field_name="full_title",
-        lookup_expr="icontains",
-        max_length=40,
-        label=_('Title'),
-    )
-
-    ordering = OrderingFilter(
-        fields=(
-            ('academic_year__year', 'academic_year'),
-            ('acronym', 'acronym'),
-            ('full_title', 'title'),
-        ),
-        widget=forms.HiddenInput
-    )
-
-    class Meta:
-        model = LearningUnitYear
-        fields = [
-            "academic_year",
-            "acronym",
-            "title",
-        ]
-
-    def __init__(self, *args, initial=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.queryset = self.get_queryset()
-        if initial:
-            self.form.fields["academic_year"].initial = initial["academic_year"]
-
-    def get_queryset(self):
-        # Need this close so as to return empty query by default when form is unbound
-        if not self.data:
-            return LearningUnitYear.objects.none()
-        queryset = LearningUnitYear.objects_with_container.select_related(
-            'academic_year',
-        )
-        queryset = LearningUnitYearQuerySet.annotate_full_title_class_method(queryset)
-        return queryset
 
 
 class QuickSearchEducationGroupYearView(PermissionRequiredMixin, CacheFilterMixin, AjaxTemplateMixin, FilterView):
