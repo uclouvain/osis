51f5a59aaf96f1db1902ad34b5204b2fbc79e09c
BenJ <benjamin.daubry@student.uclouvain.be>
2019-11-05 22:07:30 +0100

    Revert __init__ from models directories

---
+++ assessments/models/__init__.py
@@ -0,0 +1,2 @@
+from assessments.models import score_sheet_address
+from assessments.models import scores_encoding
---
+++ attribution/models/__init__.py
@@ -0,0 +1,5 @@
+from attribution.models import attribution
+from attribution.models import attribution_charge_new
+from attribution.models import attribution_new
+from attribution.models import tutor_application
+
