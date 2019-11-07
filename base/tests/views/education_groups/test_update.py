85d4f7aa3d0e35cbcb9d4158ab0bb8a0f8445164
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2019-11-07 16:27:02 +0100

    OSIS-3797 Remove update certificate aims for faculty manager

--- base/tests/views/education_groups/test_update.py
+++ base/tests/views/education_groups/test_update.py
@@ -267,44 +267,6 @@ class TestUpdate(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "education_group/update_trainings.html")
 
-    def test_template_used_for_certificate_edition(self):
-        faculty_managers_group = Group.objects.get(name='faculty_managers')
-        self.faculty_user = UserFactory()
-        self.faculty_user.groups.add(faculty_managers_group)
-        self.faculty_person = PersonFactory(user=self.faculty_user)
-        self.client.force_login(self.faculty_user)
-        permission = Permission.objects.get(codename='change_educationgroup')
-        self.faculty_user.user_permissions.add(permission)
-        self._assert_redirects_to_modal_to_update_certificate_aims(self.previous_training_education_group_year)
-        self._assert_redirects_to_modal_to_update_certificate_aims(self.training_education_group_year)
-
-        certificate_aims = [CertificateAimFactory(code=code) for code in range(100, 103)]
-        first_certificate_aim = certificate_aims[0].id
-
-        self._assert_post_to_modal_to_update_certificate_aims(
-            first_certificate_aim,
-            self.previous_training_education_group_year
-        )
-        self._assert_post_to_modal_to_update_certificate_aims(
-            first_certificate_aim,
-            self.training_education_group_year
-        )
-
-    def _assert_redirects_to_modal_to_update_certificate_aims(self, education_group_year):
-        response = self.client.get(reverse(
-            update_education_group,
-            args=[education_group_year.pk, education_group_year.pk]
-        ))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "education_group/blocks/form/training_certificate.html")
-
-    def _assert_post_to_modal_to_update_certificate_aims(self, first_certificate_aim, education_group_year):
-        response = self.client.post(reverse(update_education_group,
-                                            args=[education_group_year.pk,
-                                                  education_group_year.pk]),
-                                    data={'certificate_aims': str(first_certificate_aim)})
-        self.assertEqual(response.status_code, 302)
-
     def test_post_training(self):
         old_domain = DomainFactory()
 
