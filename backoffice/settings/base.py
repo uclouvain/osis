15de34e74b9f6509ec71f7e07b6357f4944bf8d3
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2019-11-09 14:36:25 +0100

    Ajout authentification session sur ai

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -398,6 +398,7 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
