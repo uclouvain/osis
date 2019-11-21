f133cc95b23561c5fd4b27b9e426e14da65100e8
BenJ <benjamin.daubry@student.uclouvain.be>
2019-11-22 00:21:26 +0100

    Reference (+ base)

--- reference/models/enums/__init__.py
+++ reference/models/enums/__init__.py
@@ -24,6 +24,5 @@
 #
 ##############################################################################
 from reference.models.enums import domain_type
-from reference.models.enums import education_institution_national_comunity
 from reference.models.enums import education_institution_type
 from reference.models.enums import grade_type_coverage
