ec0c2d8b9e28cfc49aaf4db83a93001386d5218c
Mathieu GERARD <mathieu.gerard@uclouvain.be>
2019-11-27 12:32:05 +0100

    OSIS-3868 Fix migration when anac does not exist

--- base/migrations/0485_create_events_lu_edition_fac_mgr.py
+++ base/migrations/0485_create_events_lu_edition_fac_mgr.py
@@ -13,17 +13,20 @@ def create_events_lu_edition_fac_mgr(apps, schema_editor):
         academic_year_mdl = apps.get_model("base", "AcademicYear")
         academic_calendar_mdl = apps.get_model("base", "AcademicCalendar")
 
-        anac = academic_year_mdl.objects.get(year=year)
-        academic_calendar_mdl.objects.create(
-            academic_year=anac,
-            data_year=anac,
-            title="Modification UE par gestionnaires facultaires",
-            description="Période de modification des Unités d'Enseignement par les gestionnaires facultaires",
-            start_date=date(year - 2, 9, 15),
-            end_date=date(year + 1, 9, 14),
-            highlight_title="Modification UE par gestionnaires facultaires",
-            reference=academic_calendar_type.LEARNING_UNIT_EDITION_FACULTY_MANAGERS
-        )
+        try:
+            anac = academic_year_mdl.objects.get(year=year)
+            academic_calendar_mdl.objects.create(
+                academic_year=anac,
+                data_year=anac,
+                title="Modification UE par gestionnaires facultaires",
+                description="Période de modification des Unités d'Enseignement par les gestionnaires facultaires",
+                start_date=date(year - 2, 9, 15),
+                end_date=date(year + 1, 9, 14),
+                highlight_title="Modification UE par gestionnaires facultaires",
+                reference=academic_calendar_type.LEARNING_UNIT_EDITION_FACULTY_MANAGERS
+            )
+        except academic_year_mdl.DoesNotExist:
+            pass
 
 
 def reverse_migration(apps, schema_editor):
@@ -31,17 +34,20 @@ def reverse_migration(apps, schema_editor):
     academic_year_mdl = apps.get_model("base", "AcademicYear")
     academic_calendar_mdl = apps.get_model("base", "AcademicCalendar")
     for year in data_years_to_delete:
-        anac = academic_year_mdl.objects.get(year=year)
-        academic_calendar_mdl.objects.filter(
-            academic_year=anac,
-            data_year=anac,
-            title="Modification UE par gestionnaires facultaires",
-            description="Période de modification des Unités d'Enseignement par les gestionnaires facultaires",
-            start_date=date(year - 2, 9, 15),
-            end_date=date(year + 1, 9, 14),
-            highlight_title="Modification UE par gestionnaires facultaires",
-            reference=academic_calendar_type.LEARNING_UNIT_EDITION_FACULTY_MANAGERS
-        ).delete()
+        try:
+            anac = academic_year_mdl.objects.get(year=year)
+            academic_calendar_mdl.objects.filter(
+                academic_year=anac,
+                data_year=anac,
+                title="Modification UE par gestionnaires facultaires",
+                description="Période de modification des Unités d'Enseignement par les gestionnaires facultaires",
+                start_date=date(year - 2, 9, 15),
+                end_date=date(year + 1, 9, 14),
+                highlight_title="Modification UE par gestionnaires facultaires",
+                reference=academic_calendar_type.LEARNING_UNIT_EDITION_FACULTY_MANAGERS
+            ).delete()
+        except academic_year_mdl.DoesNotExist:
+            pass
 
 
 class Migration(migrations.Migration):
