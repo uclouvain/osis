38c39f476d674c2b4b746578dabbe8fc1551f117
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2019-12-03 15:40:53 +0100

    OSIS-3861-dev correct modification for when Academic year not available when not in period for faculty

--- base/forms/education_group/common.py
+++ base/forms/education_group/common.py
@@ -188,7 +188,7 @@ class EducationGroupYearModelForm(ValidationRuleEducationGroupTypeMixin, Permiss
 
         self.fields['academic_year'].queryset = \
             self.fields['academic_year'].queryset.filter(year__gte=settings.YEAR_LIMIT_EDG_MODIFICATION)
-        if self.user and self.user.person.is_faculty_manager:
+        if not self.fields['academic_year'].disabled and self.user and self.user.person.is_faculty_manager:
             self.fields['academic_year'].queryset = EventPermEducationGroupEdition.get_academic_years()\
                 .filter(year__gte=settings.YEAR_LIMIT_EDG_MODIFICATION)
         self.fields['academic_year'].empty_label = None
