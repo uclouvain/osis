6241f75d7b90d16aa84153c6043597b0acf8d6bd
Ndizera Eddy <eddyndizera@gmail.com>
2019-12-04 10:31:34 +0100

    OSIS-3911 treat case when fields is disabled or absent template
    	modified:   program_management/forms/group_element_year.py
    	modified:   program_management/templates/group_element_year/group_element_year_comment_inner.html
    	modified:   program_management/tests/forms/test_group_element_year_form.py

--- program_management/tests/forms/test_group_element_year_form.py
+++ program_management/tests/forms/test_group_element_year_form.py
@@ -134,7 +134,7 @@ class TestGroupElementYearForm(TestCase):
                 form = GroupElementYearForm(parent=parent, child_branch=self.child_branch)
                 self.assertCountEqual(expected_fields, list(form.fields.keys()))
 
-    def test_only_keep_block_when_parent_is_formation_and_child_is_minor_major_option_list_choice(self):
+    def test_disable_all_except_keep_block_when_parent_is_formation_and_child_is_minor_major_option_list_choice(self):
         expected_fields = [
             "block"
         ]
@@ -146,7 +146,8 @@ class TestGroupElementYearForm(TestCase):
                     child_type=child_branch.education_group_type
                 )
                 form = GroupElementYearForm(parent=self.parent, child_branch=child_branch)
-                self.assertCountEqual(expected_fields, list(form.fields.keys()))
+                enabled_fields = [name for name, field in form.fields.items() if not field.disabled]
+                self.assertCountEqual(expected_fields, enabled_fields)
 
     def test_remove_access_condition_when_authorized_relationship(self):
         AuthorizedRelationshipFactory(
