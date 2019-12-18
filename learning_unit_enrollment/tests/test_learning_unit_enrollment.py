f7d946e606a6e4601d1011654b58f41ddaff5196
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2019-12-18 15:48:17 +0100

    OSIS-3851 usage of default test Mixin for API on 'enrollment-list-by-student' endpoint

--- learning_unit_enrollment/tests/test_learning_unit_enrollment.py
+++ learning_unit_enrollment/tests/test_learning_unit_enrollment.py
@@ -8,90 +8,62 @@ from rest_framework.test import APITestCase
 from base.models.enums import learning_unit_enrollment_state
 from base.models.learning_unit_enrollment import LearningUnitEnrollment
 from base.tests.factories.learning_unit_enrollment import LearningUnitEnrollmentFactory
-from base.tests.factories.user import UserFactory
+from base.tests.mixin.default_api_tests_cases_mixin import APIDefaultTestsCasesHttpGetMixin, APIFilterTestCaseData
 from learning_unit_enrollment.api.serializers.learning_unit_enrollment import LearningUnitEnrollmentSerializer
 
 
-class FilterEnrollmentsByStudentTestCase(APITestCase):
+class EnrollmentsListByStudentTestCase(APIDefaultTestsCasesHttpGetMixin):
 
-    def setUp(self):
-        self.offer_acronym = 'DROI1BA'
-        self.registration_id = '00000001'
-        self.url = reverse(
+    methods_not_allowed = ['post', 'delete', 'put', 'patch']
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.offer_acronym = 'DROI1BA'
+        cls.registration_id = '00000001'
+        cls.url = reverse(
             'learning_unit_enrollment_api_v1:enrollment-list-by-student',
-            kwargs={'registration_id': self.registration_id}
+            kwargs={'registration_id': cls.registration_id}
         )
-
-        self.user = UserFactory()
 
         for year in [2018, 2019, 2020]:
             LearningUnitEnrollmentFactory(
-                offer_enrollment__student__person__user=self.user,
-                offer_enrollment__student__registration_id=self.registration_id,
+                offer_enrollment__student__person__user=cls.user,
+                offer_enrollment__student__registration_id=cls.registration_id,
                 offer_enrollment__education_group_year__academic_year__year=year,
-                offer_enrollment__education_group_year__acronym=self.offer_acronym,
+                offer_enrollment__education_group_year__acronym=cls.offer_acronym,
                 learning_unit_year__academic_year__year=year,
             )
-        self.client.force_authenticate(user=self.user)
 
-    def test_get_training_case_filter_year_params(self):
-        query_string = {'year': 2018}
+    def get_filter_test_cases(self) -> List[APIFilterTestCaseData]:
+        return [
 
-        response = self.client.get(self.url, data=query_string)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+            APIFilterTestCaseData(
+                filters={'year': 2018},
+                expected_result=LearningUnitEnrollmentSerializer(
+                    LearningUnitEnrollment.objects.filter(learning_unit_year__academic_year__year=2018),
+                    many=True,
+                ).data,
+            ),
 
-        serializer = LearningUnitEnrollmentSerializer(
-            LearningUnitEnrollment.objects.filter(learning_unit_year__academic_year__year=2018),
-            many=True,
-        )
-        self.assertEqual(response.data['results'], serializer.data)
+            APIFilterTestCaseData(
+                filters={'offer_acronym': self.offer_acronym},
+                expected_result=LearningUnitEnrollmentSerializer(
+                    LearningUnitEnrollment.objects.filter(
+                        offer_enrollment__education_group_year__acronym=self.offer_acronym
+                    ),
+                    many=True,
+                ).data,
+            ),
 
-    def test_get_training_case_filter_offer_acronym_params(self):
-        query_string = {'offer_acronym': self.offer_acronym}
+            APIFilterTestCaseData(
+                filters={'enrollment_state': learning_unit_enrollment_state.ENROLLED},
+                expected_result=LearningUnitEnrollmentSerializer(
+                    LearningUnitEnrollment.objects.filter(enrollment_state=learning_unit_enrollment_state.ENROLLED),
+                    many=True,
+                ).data,
+            ),
 
-        response = self.client.get(self.url, data=query_string)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        serializer = LearningUnitEnrollmentSerializer(
-            LearningUnitEnrollment.objects.filter(offer_enrollment__education_group_year__acronym=self.offer_acronym),
-            many=True,
-        )
-        self.assertEqual(response.data['results'], serializer.data)
-
-    def test_get_training_case_filter_enrollment_state_params(self):
-        query_string = {'enrollment_state': learning_unit_enrollment_state.ENROLLED}
-
-        response = self.client.get(self.url, data=query_string)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        serializer = LearningUnitEnrollmentSerializer(
-            LearningUnitEnrollment.objects.filter(enrollment_state=learning_unit_enrollment_state.ENROLLED),
-            many=True,
-        )
-        self.assertEqual(response.data['results'], serializer.data)
-
-    # FIXME :: use mixin instead of copy-pasted code
-    def test_get_not_authorized(self):
-        self.client.force_authenticate(user=None)
-
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_get_method_not_allowed(self):
-        methods_not_allowed = ['post', 'delete', 'put', 'patch']
-
-        for method in methods_not_allowed:
-            response = getattr(self.client, method)(self.url)
-            self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    def test_pagination(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        self.assertTrue('previous' in response.data)
-        self.assertTrue('next' in response.data)
-        self.assertTrue('results' in response.data)
-        self.assertTrue('count' in response.data)
+        ]
 
 
 class LearningUnitEnrollmentSerializerTestCase(TestCase):
