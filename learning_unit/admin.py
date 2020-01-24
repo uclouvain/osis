c080ac54612200897b58ad2f8177c1f6411f3442
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-01-24 13:29:48 -0800

    OSIS-4170 : Useless

--- learning_unit/admin.py
+++ learning_unit/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from learning_unit.models import learning_class_year
+from learning_unit.models import *
 
 admin.site.register(learning_class_year.LearningClassYear,
                     learning_class_year.LearningClassYearAdmin)
--- learning_unit/models/learning_class_year.py
+++ learning_unit/models/learning_class_year.py
@@ -27,10 +27,10 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from osis_common.models import osis_model_admin
+from osis_common.models.osis_model_admin import OsisModelAdmin
 
 
-class LearningClassYearAdmin(osis_model_admin.OsisModelAdmin):
+class LearningClassYearAdmin(OsisModelAdmin):
     list_display = ('learning_component_year', 'acronym')
     search_fields = ['acronym']
 
