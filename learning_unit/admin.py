c81c5f2a3b1bed0a028464f63c77a85a182d145f
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-01-24 11:39:23 -0800

    OSIS-3943 : Move LearningClassYear admin registration from base to learning_unit

--- learning_unit/admin.py
+++ learning_unit/admin.py
@@ -1,1 +1,6 @@
-# Register your models here.
+from django.contrib import admin
+
+from learning_unit.models import learning_class_year
+
+admin.site.register(learning_class_year.LearningClassYear,
+                    learning_class_year.LearningClassYearAdmin)
