a121dcb970024342a6ae6d0a0d15f24a23493416
Christophe Fivet <christophe.fivet@uclouvain.be>
2020-01-28 17:35:08 +0100

    Déclaration du reverse

--- base/migrations/0499_auto_20200128_1134.py
+++ base/migrations/0499_auto_20200128_1134.py
@@ -55,5 +55,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(create_partial_title),
+        migrations.RunPython(create_partial_title, reverse_migration),
     ]
