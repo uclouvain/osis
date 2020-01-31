831882c7da19fc9bbccf871dd4317bbd4e5c5d54
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2020-01-31 08:58:31 +0100

    OSIS-4114-dev add error message in assert

--- base/tests/forms/test_learning_unit_edition.py
+++ base/tests/forms/test_learning_unit_edition.py
@@ -100,5 +100,5 @@ class TestLearningUnitEditionForm(TestCase, LearningUnitsMixin):
 
         for case in cases:
             with self.subTest():
-                self.assertEqual(case["expected_result"],
-                                 get_next_academic_years(case["learning_unit"], case["last_year"]).count())
+                result = get_next_academic_years(case["learning_unit"], case["last_year"])
+                self.assertEqual(case["expected_result"], result.count(), result)
