d7928bcd35c670e8fe020eba5b8d9b92e7ca0502
Ndizera Eddy <eddyndizera@gmail.com>
2020-02-19 10:58:51 +0100

    OSIS-4203 refactor navigation templates
    	modified:   base/templatetags/navigation.py

--- base/templatetags/navigation.py
+++ base/templatetags/navigation.py
@@ -34,45 +34,49 @@ from base.forms.education_groups import EducationGroupFilter
 
 @register.inclusion_tag('templatetags/navigation.html', takes_context=False)
 def navigation(query_parameters: QueryDict, current_element):
+    context = {"current_element": current_element}
+
     search_query_string = query_parameters.get("search_query")
     index = query_parameters.get("index")
-    context = {"current_element": current_element}
     if search_query_string and index is not None:
         unquoted_search_query_string = urllib.parse.unquote_plus(search_query_string)
-        context.update(update_context_with_navigation_elements(query_parameters, unquoted_search_query_string, int(index)))
+        context.update(get_neighbor_elements(query_parameters, unquoted_search_query_string, int(index)))
+
     return context
 
 
-def update_context_with_navigation_elements(query_parameters, search_query_string, index,):
+def get_neighbor_elements(query_parameters, search_query_string, index, ):
     search_parameters = QueryDict(search_query_string).dict()
     qs = EducationGroupFilter(data=search_parameters).qs
-    context = {}
-    context["next_element"] = get_next_element(qs, index)
-    context["next_url"] = create_url(context["next_element"], query_parameters, index + 1) if context["next_element"] else None
-    context["previous_element"] = get_previous_element(qs, index)
-    context["previous_url"] = create_url(context["previous_element"], query_parameters, index - 1) if context["previous_element"] else None
-    return context
+    next_element = _get_next_element(qs, index)
+    previous_element = _get_previous_element(qs, index)
+    return {
+        "next_element": next_element,
+        "next_url": _create_url(next_element, query_parameters, index + 1) if next_element else None,
+        "previous_element": previous_element,
+        "previous_url": _create_url(previous_element, query_parameters, index - 1) if previous_element else None
+    }
 
 
-def get_next_element(qs, index):
+def _get_next_element(qs, index):
     try:
         return qs[index + 1] if index >= 0 else None
     except IndexError:
         return None
 
 
-def get_previous_element(qs, index):
+def _get_previous_element(qs, index):
     try:
         return qs[index - 1] if index > 0 else None
     except IndexError:
         return None
 
 
-def create_url(education_group_year, query_parameters: QueryDict, index):
-    new_parameters = QueryDict(mutable=True)
-    new_parameters.update(query_parameters)
-    new_parameters["index"] = index
+def _create_url(education_group_year, query_parameters: QueryDict, index):
+    query_parameters_with_udpated_index = QueryDict(mutable=True)
+    query_parameters_with_udpated_index.update(query_parameters)
+    query_parameters_with_udpated_index["index"] = index
     return "{}?{}".format(
         reverse("education_group_read", args=[education_group_year.id, education_group_year.id]),
-        new_parameters.urlencode()
+        query_parameters_with_udpated_index.urlencode()
     )
