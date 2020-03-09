9a8eb691c749f0a173cd5cf7de255efbe4146ee7
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-03-09 14:08:42 +0100

    OSIS-4411

--- education_group/tests/api/serializers/test_training.py
+++ education_group/tests/api/serializers/test_training.py
@@ -29,7 +29,7 @@ from django.urls import reverse
 
 from base.models.enums import organization_type, education_group_types
 from base.tests.factories.academic_year import AcademicYearFactory
-from base.tests.factories.education_group_year import TrainingFactory
+from base.tests.factories.education_group_year import TrainingFactory, EducationGroupYearBachelorFactory
 from base.tests.factories.entity_version import EntityVersionFactory
 from education_group.api.serializers.training import TrainingListSerializer, TrainingDetailSerializer
 from reference.tests.factories.domain import DomainFactory
@@ -42,7 +42,7 @@ class TrainingListSerializerTestCase(TestCase):
         cls.entity_version = EntityVersionFactory(
             entity__organization__type=organization_type.MAIN
         )
-        cls.training = TrainingFactory(
+        cls.training = EducationGroupYearBachelorFactory(
             acronym='BIR1BA',
             partial_acronym='LBIR1000I',
             academic_year=cls.academic_year,
@@ -66,7 +66,6 @@ class TrainingListSerializerTestCase(TestCase):
             'academic_year',
             'administration_entity',
             'management_entity',
-            'partial_title',
         ]
         self.assertListEqual(list(self.serializer.data), expected_fields)
 
@@ -140,7 +139,7 @@ class TrainingDetailSerializerTestCase(TestCase):
         cls.entity_version = EntityVersionFactory(
             entity__organization__type=organization_type.MAIN
         )
-        cls.training = TrainingFactory(
+        cls.training = EducationGroupYearBachelorFactory(
             acronym='BIR1BA',
             partial_acronym='LBIR1000I',
             academic_year=cls.academic_year,
