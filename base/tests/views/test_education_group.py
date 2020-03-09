570b97f44331feeed890046371ab235c0d6b528e
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-03-09 14:49:56 +0100

    OSIS-4411

--- base/tests/views/test_education_group.py
+++ base/tests/views/test_education_group.py
@@ -383,8 +383,9 @@ class EducationGroupViewTestCase(TestCase):
         self.assertEqual(response.context['parent'], an_education_group)
 
     def test_education_administrative_data_with_root_set(self):
+        edy = TrainingFactory(academic_year=self.academic_year)
         a_group_element_year = GroupElementYearFactory(parent__academic_year=self.academic_year,
-                                                       child_branch__academic_year=self.academic_year)
+                                                       child_branch=edy)
         self.initialize_session()
         url = reverse("education_group_administrative",
                       args=[a_group_element_year.parent.id, a_group_element_year.child_branch.id])
