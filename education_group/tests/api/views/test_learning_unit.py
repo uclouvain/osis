1fe6d0549cfe7ac2bfd857a01130a5e455213d53
BenJ <benjamin.daubry@uclouvain.be>
2020-03-12 09:49:01 +0100

    OSIS-4310 : Filter finality and get root

--- education_group/tests/api/views/test_learning_unit.py
+++ education_group/tests/api/views/test_learning_unit.py
@@ -30,9 +30,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from base.models.education_group_year import EducationGroupYear
-from base.models.enums.education_group_types import GroupType
+from base.models.enums.education_group_types import GroupType, TrainingType
 from base.tests.factories.academic_year import AcademicYearFactory, create_current_academic_year
-from base.tests.factories.education_group_year import TrainingFactory, GroupFactory
+from base.tests.factories.education_group_year import TrainingFactory, GroupFactory, EducationGroupYearMasterFactory
 from base.tests.factories.group_element_year import GroupElementYearFactory
 from base.tests.factories.learning_unit_year import LearningUnitYearFactory
 from base.tests.factories.person import PersonFactory
@@ -50,7 +50,9 @@ class FilterEducationGroupRootsTestCase(APITestCase):
         cls.user = UserFactory()
 
         cls.academic_year = create_current_academic_year()
-        cls.training = TrainingFactory(academic_year=cls.academic_year)
+        cls.training = EducationGroupYearMasterFactory(
+            academic_year=cls.academic_year, acronym='test2m', partial_acronym='test2m'
+        )
         cls.common_core = GroupFactory(
             education_group_type__name=GroupType.COMMON_CORE.name,
             academic_year=cls.academic_year
@@ -71,6 +73,7 @@ class FilterEducationGroupRootsTestCase(APITestCase):
         )
         GroupElementYearFactory(parent=cls.common_core, child_branch=None, child_leaf=cls.learning_unit_year)
         GroupElementYearFactory(parent=cls.complementary_module, child_branch=None, child_leaf=cls.learning_unit_year)
+
         cls.person = PersonFactory()
         url_kwargs = {
             'acronym': cls.learning_unit_year.acronym,
@@ -97,6 +100,33 @@ class FilterEducationGroupRootsTestCase(APITestCase):
                 'language': settings.LANGUAGE_CODE_FR
             }
         )
+
+        self.assertEqual(response.data, serializer.data)
+
+    def test_get_finality_root_and_not_itself(self):
+        finality_root = EducationGroupYearMasterFactory(academic_year=self.academic_year)
+        finality = TrainingFactory(
+            academic_year=self.academic_year,
+            education_group_type__name=TrainingType.MASTER_MD_120.name
+        )
+        GroupElementYearFactory(parent=finality_root, child_branch=finality, child_leaf=None)
+        GroupElementYearFactory(parent=finality, child_branch=None, child_leaf=self.learning_unit_year)
+
+        query_string = {'ignore_complementary_module': 'true'}
+        response = self.client.get(self.url, data=query_string)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        education_group_roots = EducationGroupYear.objects.filter(id=self.training.id)
+
+        serializer = EducationGroupRootsListSerializer(
+            list(education_group_roots) + [finality_root],
+            many=True,
+            context={
+                'request': RequestFactory().get(self.url),
+                'language': settings.LANGUAGE_CODE_FR
+            }
+        )
+
         self.assertEqual(response.data, serializer.data)
 
 
