2b48df2da28a301e891d15856fcef275dac146b9
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2020-04-06 15:48:21 +0200

    OSIS-4515 Migrate user from faculty_manager/central_manager/sic to new role model

--- base/models/education_group.py
+++ base/models/education_group.py
@@ -109,8 +109,6 @@ class EducationGroup(SerializableModel):
             ("add_minitraining", "Can add mini-training"),
             ("add_group", "Can add group"),
             ("change_commonpedagogyinformation", "Can change common pedagogy information"),
-            ("change_commonpedagogyinformation", "Can change common pedagogy information"),
-            ("change_commonpedagogyinformation", "Can change common pedagogy information"),
             ("change_pedagogyinformation", "Can change pedagogy information"),
             ("change_educationgroupcontent", "Can change education group content"),
         )
