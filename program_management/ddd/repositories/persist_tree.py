5aad79741cfc3ee7b4797ae4c2d266cef34b6329
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2020-04-21 17:03:24 +0200

    OSIS-4527 fixed failing tests

--- program_management/ddd/repositories/persist_tree.py
+++ program_management/ddd/repositories/persist_tree.py
@@ -70,7 +70,8 @@ def __persist_group_element_year(link):
 
 def __delete_links(tree: program_tree.ProgramTree, node: Node):
     for link in node._deleted_children:
-        _persist_prerequisite._persist(tree.root_node, link.child)  # TODO :: unit tests
+        if link.child.is_learning_unit():
+            _persist_prerequisite._persist(tree.root_node, link.child)
         __delete_group_element_year(link)
     for link in node.children:
         __delete_links(tree, link.child)
