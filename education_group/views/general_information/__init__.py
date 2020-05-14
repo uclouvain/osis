6c3b6ac80ac72929bb021a71407f25f87375ec5e
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2020-05-14 17:20:08 +0200

    Add squelet of common structur

---
+++ education_group/views/general_information/__init__.py
@@ -0,0 +1,5 @@
+from .common import CommonGeneralInformation
+from .common_bachelor import CommonBachelorAdmissionCondition
+from .common_aggregate import CommonAggregateAdmissionCondition
+from .common_master import CommonMasterAdmissionCondition
+from .common_master_specialized import CommonMasterSpecializedAdmissionCondition
