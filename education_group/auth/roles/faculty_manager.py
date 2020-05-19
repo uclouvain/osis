b377a0024b7c08d517bd0e8e73059092aac5ccfa
Mathieu Zen <zen.mathieu@gmail.com>
2020-05-19 14:17:42 +0200

    OSIS-3717 enable faculty manager to add minitraining/group only if manage all scopes in entity

--- education_group/auth/roles/faculty_manager.py
+++ education_group/auth/roles/faculty_manager.py
@@ -38,11 +38,15 @@ class FacultyManager(EducationGroupTypeScopeRoleMixin, osis_role_models.EntityRo
             ),
             'base.add_minitraining':
                 predicates.is_user_attached_to_management_entity &
+                predicates.is_user_linked_to_all_scopes_of_management_entity &
+                predicates.is_education_group_year_older_or_equals_than_limit_settings_year &
                 predicates.is_program_edition_period_open &
                 predicates.is_maximum_child_not_reached_for_mini_training_category,
             'base.add_group':
                 predicates.is_user_attached_to_management_entity &
                 predicates.is_not_orphan_group &
+                predicates.is_user_linked_to_all_scopes_of_management_entity &
+                predicates.is_education_group_year_older_or_equals_than_limit_settings_year &
                 predicates.is_program_edition_period_open &
                 predicates.is_maximum_child_not_reached_for_group_category,
             # TODO : split in training, minitraining, group
