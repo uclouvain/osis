5722759841e280663a971e70cd8592217b6de928
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2020-05-28 15:30:35 +0200

    OSIS-4384 Add Test on Group Identificaiton Read

--- education_group/views/group/identification_read.py
+++ education_group/views/group/identification_read.py
@@ -1,5 +1,6 @@
 from reversion.models import Version
 
+from education_group.models.group_year import GroupYear
 from education_group.views.group.common_read import Tab, GroupRead
 
 
@@ -10,8 +11,25 @@ class GroupReadIdentification(GroupRead):
     def get_context_data(self, **kwargs):
         return {
             **super().get_context_data(**kwargs),
-            "versions": self.get_related_versions(),
+            "history": self.get_related_history(),
         }
 
-    def get_related_versions(self):
-        return Version.objects.none()
+    def get_related_history(self):
+        group_year = self.get_group_year()
+        versions = Version.objects.get_for_object(
+            group_year
+        ).select_related('revision__user__person')
+
+        related_models = [
+            GroupYear,
+        ]
+
+        subversion = Version.objects.none()
+        for model in related_models:
+            subversion |= Version.objects.get_for_model(model).select_related('revision__user__person')
+
+        versions |= subversion.filter(
+            serialized_data__contains="\"group_year\": {}".format(group_year.pk)
+        )
+
+        return versions.order_by('-revision__date_created').distinct('revision__date_created')
