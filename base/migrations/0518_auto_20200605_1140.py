281b6c8175cda1ee5c881cd46c54054fc1dca69f
Mathieu Zen <zen.mathieu@gmail.com>
2020-06-18 15:44:34 +0200

    OSIS-4655 rename migration file

--- base/migrations/0518_auto_20200605_1140.py
+++ base/migrations/0518_auto_20200605_1140.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0516_auto_20200602_1440'),
+        ('base', '0517_entityversionaddress'),
     ]
 
     operations = [
