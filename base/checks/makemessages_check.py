25f320e9a617dc43c6578bd40061bc570cef4b94
Ndizera Eddy <eddyndizera@gmail.com>
2020-07-02 13:36:31 +0200

    created makemessages check empty function

---
+++ base/checks/makemessages_check.py
@@ -0,0 +1,39 @@
+# ############################################################################
+#  OSIS stands for Open Student Information System. It's an application
+#  designed to manage the core business of higher education institutions,
+#  such as universities, faculties, institutes and professional schools.
+#  The core business involves the administration of students, teachers,
+#  courses, programs and so on.
+#
+#  Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of this license - GNU General Public License - is available
+#  at the root of the source code of this program.  If not,
+#  see http://www.gnu.org/licenses/.
+# ############################################################################
+from typing import List
+
+from django.core.checks import register, Tags, CheckMessage
+
+
+@register(Tags.translation)
+def makemessages_check(app_configs, **kwargs) -> List[CheckMessage]:
+    error = []
+    all_aps =
+
+    return error
+
+
+def find_all_aps():
+    working_directory = "."
+
