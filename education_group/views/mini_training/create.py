f0c29a81a7b0bab39b7641c71d7030eb0664e529
Ndizera Eddy <eddyndizera@gmail.com>
2020-07-03 09:38:41 +0200

    OSIS-4727 create orphan mini training command

--- education_group/views/mini_training/create.py
+++ education_group/views/mini_training/create.py
@@ -23,12 +23,14 @@
 # ############################################################################
 from typing import List, Dict, Type
 
+from django.http import response
 from django.urls import reverse
 from django.views.generic import FormView
 from django.utils.translation import gettext_lazy as _
 from rules.contrib.views import LoginRequiredMixin
 
 from base.models.enums.education_group_types import MiniTrainingType
+from education_group.ddd import command
 from education_group.ddd.service.write import create_mini_training_service
 from education_group.forms import mini_training as mini_training_form
 from osis_role.contrib.views import PermissionRequiredMixin
@@ -49,6 +51,7 @@ class MiniTrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, FormVi
         form_kwargs["mini_training_type"] = self.kwargs['type']
         return form_kwargs
 
+    #  TODO incorporate type in mini training form
     def get_context_data(self, **kwargs) -> Dict:
         context = super().get_context_data(**kwargs)
         context["mini_training_form"] = context["form"]
@@ -56,8 +59,29 @@ class MiniTrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, FormVi
         context["type_text"] = MiniTrainingType.get_value(self.kwargs['type'])
         return context
 
-    def form_valid(self, form):
-        create_mini_training_service.create_orphan_mini_training(None)
+    def form_valid(self, form: mini_training_form.MiniTrainingForm) -> response.HttpResponseBase:
+        create_command = command.CreateOrphanMiniTrainingCommand(
+            code=form.cleaned_data['code'],
+            year=form.cleaned_data['year'],
+            type=self.kwargs['type'],
+            abbreviated_title=form.cleaned_data['abbreviated_title'],
+            title_fr=form.cleaned_data['title_fr'],
+            title_en=form.cleaned_data['title_en'],
+            status=form.cleaned_data['status'],
+            schedule_type=form.cleaned_data['schedule_type'],
+            credits=form.cleaned_data['credits'],
+            constraint_type=form.cleaned_data['constraint_type'],
+            min_constraint=form.cleaned_data['min_constraint'],
+            max_constraint=form.cleaned_data['max_constraint'],
+            management_entity_acronym=form.cleaned_data['management_entity_acronym'],
+            teaching_campus_name=form.cleaned_data['teaching_campus_name'],
+            organization_name=form.cleaned_data['organization_name'],
+            remark_fr=form.cleaned_data['remark_fr'],
+            remark_en=form.cleaned_data['remark_en'],
+            start_year=form.cleaned_data['start_year'],
+            end_year=form.cleaned_data['end_year'],
+        )
+        create_mini_training_service.create_orphan_mini_training(create_command)
         return super().form_valid(form)
 
     def get_tabs(self) -> List:
@@ -70,5 +94,5 @@ class MiniTrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, FormVi
             }
         ]
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return reverse("home")
