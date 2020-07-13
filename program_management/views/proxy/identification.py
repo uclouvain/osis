78a3651a0dff59d2cfca86f2e8d82a218349af31
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-07-13 08:36:05 +0200

    OSIS-4722 : Chnager l'url en fonction de la page de laquelle on vient

--- program_management/views/proxy/identification.py
+++ program_management/views/proxy/identification.py
@@ -4,6 +4,10 @@ from django.views.generic import RedirectView
 from program_management.ddd.domain.node import NodeIdentity
 from program_management.ddd.repositories.node import NodeRepository
 
+from education_group.views.proxy.read import SUFFIX_CONTENT, SUFFIX_ADMINISTRATIVE_DATA, SUFFIX_DIPLOMAS_CERTIFICATES, \
+    SUFFIX_ADMISSION_CONDITION, SUFFIX_SKILLS_ACHIEVEMENTS, SUFFIX_IDENTIFICATION, SUFFIX_UTILIZATION, \
+    SUFFIX_GENERAL_INFO
+
 
 class IdentificationRedirectView(RedirectView):
     permanent = False
@@ -14,16 +18,28 @@ class IdentificationRedirectView(RedirectView):
         code = self.kwargs['code']
         root_node = NodeRepository().get(NodeIdentity(code=code, year=year))
         if root_node.is_training():
-            url_name = "training_identification"
+            url_name_prefix = "training"
             url_kwargs = {'year': root_node.year, 'code': root_node.code}
         elif root_node.is_mini_training():
-            url_name = "mini_training_identification"
+            url_name_prefix = "mini_training"
             url_kwargs = {'year': root_node.year, 'code': root_node.code}
         elif root_node.is_learning_unit():
-            url_name = "learning_unit"
+            url_name_prefix = "learning_unit"
             url_kwargs = {'year': root_node.year, 'acronym': root_node.code}
         else:
-            url_name = "group_identification"
+            url_name_prefix = "group"
             url_kwargs = {'year': root_node.year, 'code': root_node.code}
-        self.url = reverse(url_name, kwargs=url_kwargs)
+        self.url = reverse(
+            "{}_{}".format(url_name_prefix, get_url_name_suffix_from_referer(self.request.META.get('HTTP_REFERER'))),
+            kwargs=url_kwargs
+        )
         return super().get_redirect_url(*args, **kwargs)
+
+
+def get_url_name_suffix_from_referer(referer: str):
+    url_name_suffixes = [SUFFIX_CONTENT, SUFFIX_ADMINISTRATIVE_DATA, SUFFIX_DIPLOMAS_CERTIFICATES,
+                         SUFFIX_ADMISSION_CONDITION, SUFFIX_SKILLS_ACHIEVEMENTS, SUFFIX_IDENTIFICATION,
+                         SUFFIX_UTILIZATION, SUFFIX_GENERAL_INFO]
+    if referer:
+        return next((suffix for suffix in url_name_suffixes if suffix in referer), SUFFIX_IDENTIFICATION)
+    return SUFFIX_IDENTIFICATION
