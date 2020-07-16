a3bee754f35888d270e607fd9ccfc5e9e4ac6bb2
BenJ <benjamin.daubry@uclouvain.be>
2020-07-16 10:57:09 +0200

    OSIS-4872 : Fix test_group_element_year.py

--- education_group/tests/api/views/test_group_element_year.py
+++ education_group/tests/api/views/test_group_element_year.py
@@ -199,7 +199,7 @@ class MiniTrainingTreeViewTestCase(APITestCase):
         cls.mini_training_version = StandardEducationGroupVersionFactory(
             offer=cls.mini_training,
             root_group__education_group_type=cls.mini_training.education_group_type,
-            root_group__partial_acronym='LBIOL212O',
+            root_group__partial_acronym=cls.mini_training.partial_acronym,
             root_group__academic_year=cls.academic_year,
             is_transition=False
         )
