2fa56c65a1f07470ca716d7de97cf7f5de11330f
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2020-07-17 13:23:37 +0200

    OSIS-4728 add service to create and attach training

--- education_group/views/training/create.py
+++ education_group/views/training/create.py
@@ -14,10 +14,11 @@ from base.models.enums.education_group_types import GroupType, TrainingType
 from base.utils.cache import RequestCache
 from base.views.common import display_success_messages
 from education_group.ddd import command
+from program_management.ddd import command as program_management_command
 from education_group.ddd.domain.exception import GroupCodeAlreadyExistException, ContentConstraintTypeMissing, \
     ContentConstraintMinimumMaximumMissing, ContentConstraintMaximumShouldBeGreaterOrEqualsThanMinimum
 from education_group.ddd.domain.training import TrainingIdentity
-from program_management.ddd.service.write import create_training_with_program_tree
+from program_management.ddd.service.write import create_training_with_program_tree, create_and_attach_training_service
 from education_group.forms.training import CreateTrainingForm
 from education_group.templatetags.academic_year_display import display_as_academic_year
 from education_group.views.proxy.read import Tab
@@ -59,16 +60,19 @@ class TrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         training_form = CreateTrainingForm(data=request.POST, user=self.request.user, training_type=self.kwargs['type'])
         if training_form.is_valid():
-            create_training_cmd = _convert_training_form_to_command(training_form)
+            create_training_data = _convert_training_form_to_data_for_service(training_form)
             try:
                 if self.get_attach_path():
                     training_ids = None
-                    # create_and_attach_training_cmd = CreateAndAttachTrainingCommand(
-                    #     **create_training_cmd
-                    # )
-                    # training_id = create_and_attach_training_service.create_and_attach_training(create_training_cmd)
+                    cmd = program_management_command.CreateAndAttachTrainingCommand(
+                        **create_training_data,
+                        path_to_paste=self.get_attach_path(),
+                    )
+                    training_ids = create_and_attach_training_service.create_and_attach_training(cmd)
                 else:
-                    training_ids = create_and_report_training_with_program_tree(create_training_cmd)
+                    training_ids = create_and_report_training_with_program_tree(
+                        command.CreateTrainingCommand(**create_training_data)
+                    )
 
             except GroupCodeAlreadyExistException as e:
                 training_form.add_error('code', e.message)
@@ -136,7 +140,7 @@ class TrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, View):
             # Ex:  path: 4456|565|5656
             parent_id = path.split("|")[-1]
             qs = qs.filter(
-                educationgroupversion__root_group__element_id=parent_id,
+                educationgroupversion__root_group__element__id=parent_id,
             )
         else:
             qs = qs.filter(
@@ -149,73 +153,73 @@ class TrainingCreateView(LoginRequiredMixin, PermissionRequiredMixin, View):
             return None
 
 
-def _convert_training_form_to_command(training_form: CreateTrainingForm) -> command.CreateTrainingCommand:
-    return command.CreateTrainingCommand(
-        abbreviated_title=training_form.cleaned_data['acronym'],
-        status=training_form.cleaned_data['active'],
-        code=training_form.cleaned_data['code'],
-        year=training_form.cleaned_data['academic_year'].year,
-        type=training_form.training_type,
-        credits=training_form.cleaned_data['credits'],
-        schedule_type=training_form.cleaned_data['schedule_type'],
-        duration=training_form.cleaned_data['duration'],
-        start_year=training_form.cleaned_data['academic_year'].year,
-        title_fr=training_form.cleaned_data['title_fr'],
-        partial_title_fr=training_form.cleaned_data['partial_title_fr'],
-        title_en=training_form.cleaned_data['title_en'],
-        partial_title_en=training_form.cleaned_data['partial_title_en'],
-        keywords=training_form.cleaned_data['keywords'],
-        internship=training_form.cleaned_data['internship'],
-        is_enrollment_enabled=training_form.cleaned_data['is_enrollment_enabled'],
-        has_online_re_registration=training_form.cleaned_data['has_online_re_registration'],
-        has_partial_deliberation=training_form.cleaned_data['has_partial_deliberation'],
-        has_admission_exam=training_form.cleaned_data['has_admission_exam'],
-        has_dissertation=training_form.cleaned_data['has_dissertation'],
-        produce_university_certificate=training_form.cleaned_data['produce_university_certificate'],
-        decree_category=training_form.cleaned_data['decree_category'],
-        rate_code=training_form.cleaned_data['rate_code'],
-        main_language=training_form.cleaned_data['main_language'],
-        english_activities=training_form.cleaned_data['english_activities'],
-        other_language_activities=training_form.cleaned_data['other_language_activities'],
-        internal_comment=training_form.cleaned_data['internal_comment'],
-        main_domain_code=training_form.cleaned_data['main_domain'].code
+def _convert_training_form_to_data_for_service(training_form: CreateTrainingForm) -> Dict:
+    return {
+        'abbreviated_title': training_form.cleaned_data['acronym'],
+        'status': training_form.cleaned_data['active'],
+        'code': training_form.cleaned_data['code'],
+        'year': training_form.cleaned_data['academic_year'].year,
+        'type': training_form.training_type,
+        'credits': training_form.cleaned_data['credits'],
+        'schedule_type': training_form.cleaned_data['schedule_type'],
+        'duration': training_form.cleaned_data['duration'],
+        'start_year': training_form.cleaned_data['academic_year'].year,
+        'title_fr': training_form.cleaned_data['title_fr'],
+        'partial_title_fr': training_form.cleaned_data['partial_title_fr'],
+        'title_en': training_form.cleaned_data['title_en'],
+        'partial_title_en': training_form.cleaned_data['partial_title_en'],
+        'keywords': training_form.cleaned_data['keywords'],
+        'internship': training_form.cleaned_data['internship'],
+        'is_enrollment_enabled': training_form.cleaned_data['is_enrollment_enabled'],
+        'has_online_re_registration': training_form.cleaned_data['has_online_re_registration'],
+        'has_partial_deliberation': training_form.cleaned_data['has_partial_deliberation'],
+        'has_admission_exam': training_form.cleaned_data['has_admission_exam'],
+        'has_dissertation': training_form.cleaned_data['has_dissertation'],
+        'produce_university_certificate': training_form.cleaned_data['produce_university_certificate'],
+        'decree_category': training_form.cleaned_data['decree_category'],
+        'rate_code': training_form.cleaned_data['rate_code'],
+        'main_language': training_form.cleaned_data['main_language'],
+        'english_activities': training_form.cleaned_data['english_activities'],
+        'other_language_activities': training_form.cleaned_data['other_language_activities'],
+        'internal_comment': training_form.cleaned_data['internal_comment'],
+        'main_domain_code': training_form.cleaned_data['main_domain'].code
         if training_form.cleaned_data['main_domain'] else None,
-        main_domain_decree=training_form.cleaned_data['main_domain'].decree.name
+        'main_domain_decree': training_form.cleaned_data['main_domain'].decree.name
         if training_form.cleaned_data['main_domain'] else None,
-        secondary_domains=[
+        'secondary_domains': [
             (obj.decree.name, obj.code) for obj in training_form.cleaned_data['secondary_domains']
         ],
-        isced_domain_code=training_form.cleaned_data['isced_domain'].code
+        'isced_domain_code': training_form.cleaned_data['isced_domain'].code
         if training_form.cleaned_data['isced_domain'] else None,
-        management_entity_acronym=training_form.cleaned_data['management_entity'],
-        administration_entity_acronym=training_form.cleaned_data['administration_entity'],
-        end_year=training_form.cleaned_data['end_year'].year
+        'management_entity_acronym': training_form.cleaned_data['management_entity'],
+        'administration_entity_acronym': training_form.cleaned_data['administration_entity'],
+        'end_year': training_form.cleaned_data['end_year'].year
         if training_form.cleaned_data['end_year'] else None,
-        teaching_campus_name=training_form.cleaned_data['teaching_campus'].name,
-        teaching_campus_organization_name=training_form.cleaned_data['teaching_campus'].organization.name,
-        enrollment_campus_name=training_form.cleaned_data['enrollment_campus'].name,
-        enrollment_campus_organization_name=training_form.cleaned_data['enrollment_campus'].organization.name,
-        other_campus_activities=training_form.cleaned_data['other_campus_activities'],
-        can_be_funded=training_form.cleaned_data['can_be_funded'],
-        funding_orientation=training_form.cleaned_data['funding_direction'],
-        can_be_international_funded=training_form.cleaned_data['can_be_international_funded'],
-        international_funding_orientation=training_form.cleaned_data['international_funding_orientation'],
-        ares_code=training_form.cleaned_data['ares_code'],
-        ares_graca=training_form.cleaned_data['ares_graca'],
-        ares_authorization=training_form.cleaned_data['ares_authorization'],
-        code_inter_cfb=training_form.cleaned_data['code_inter_cfb'],
-        coefficient=training_form.cleaned_data['coefficient'],
-        academic_type=training_form.cleaned_data['academic_type'],
-        duration_unit=training_form.cleaned_data['duration_unit'],
-        leads_to_diploma=training_form.cleaned_data['leads_to_diploma'],
-        printing_title=training_form.cleaned_data['diploma_printing_title'],
-        professional_title=training_form.cleaned_data['professional_title'],
-        aims=[
+        'teaching_campus_name': training_form.cleaned_data['teaching_campus'].name,
+        'teaching_campus_organization_name': training_form.cleaned_data['teaching_campus'].organization.name,
+        'enrollment_campus_name': training_form.cleaned_data['enrollment_campus'].name,
+        'enrollment_campus_organization_name': training_form.cleaned_data['enrollment_campus'].organization.name,
+        'other_campus_activities': training_form.cleaned_data['other_campus_activities'],
+        'can_be_funded': training_form.cleaned_data['can_be_funded'],
+        'funding_orientation': training_form.cleaned_data['funding_direction'],
+        'can_be_international_funded': training_form.cleaned_data['can_be_international_funded'],
+        'international_funding_orientation': training_form.cleaned_data['international_funding_orientation'],
+        'ares_code': training_form.cleaned_data['ares_code'],
+        'ares_graca': training_form.cleaned_data['ares_graca'],
+        'ares_authorization': training_form.cleaned_data['ares_authorization'],
+        'code_inter_cfb': training_form.cleaned_data['code_inter_cfb'],
+        'coefficient': training_form.cleaned_data['coefficient'],
+        'academic_type': training_form.cleaned_data['academic_type'],
+        'duration_unit': training_form.cleaned_data['duration_unit'],
+        'leads_to_diploma': training_form.cleaned_data['leads_to_diploma'],
+        'printing_title': training_form.cleaned_data['diploma_printing_title'],
+        'professional_title': training_form.cleaned_data['professional_title'],
+        'aims': [
             (aim.code, aim.section) for aim in (training_form.cleaned_data['certificate_aims'] or [])
         ],
-        constraint_type=training_form.cleaned_data['constraint_type'],
-        min_constraint=training_form.cleaned_data['min_constraint'],
-        max_constraint=training_form.cleaned_data['max_constraint'],
-        remark_fr=training_form.cleaned_data['remark_fr'],
-        remark_en=training_form.cleaned_data['remark_english'],
-    )
+        'constraint_type': training_form.cleaned_data['constraint_type'],
+        'min_constraint': training_form.cleaned_data['min_constraint'],
+        'max_constraint': training_form.cleaned_data['max_constraint'],
+        'remark_fr': training_form.cleaned_data['remark_fr'],
+        'remark_en': training_form.cleaned_data['remark_english'],
+    }
