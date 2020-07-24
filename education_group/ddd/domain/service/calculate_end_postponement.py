34fd5f677a90d9e9f481a666837a4056028c68e0
Ndizera Eddy <eddyndizera@gmail.com>
2020-07-24 10:42:43 +0200

    OSIS-4727 rename parameter

--- education_group/ddd/domain/service/calculate_end_postponement.py
+++ education_group/ddd/domain/service/calculate_end_postponement.py
@@ -39,9 +39,9 @@ class CalculateEndPostponement(interface.DomainService):
         return current_year + default_years_to_postpone
 
     @classmethod
-    def calculate_year_of_end_postponement_for_mini(cls, training: 'MiniTraining') -> int:
+    def calculate_year_of_end_postponement_for_mini(cls, mini_training: 'MiniTraining') -> int:
         default_years_to_postpone = 6
         current_year = academic_year.starting_academic_year().year
-        if training.end_year:
-            default_years_to_postpone = training.end_year - training.year
+        if mini_training.end_year:
+            default_years_to_postpone = mini_training.end_year - mini_training.year
         return current_year + default_years_to_postpone
