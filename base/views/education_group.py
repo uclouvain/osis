0a1bf6ada40825fe19bb77aab651dc9a8a80415d
BenJ <benjamin.daubry@uclouvain.be>
2020-07-31 10:27:40 +0200

    OSIS-4920 : Use repositories

--- base/views/education_group.py
+++ base/views/education_group.py
@@ -48,7 +48,8 @@ from education_group.ddd.domain.service.identity_search import TrainingIdentityS
 from education_group.views.proxy.read import Tab
 from osis_common.decorators.ajax import ajax_required
 from program_management.ddd.domain.node import Node
-from program_management.ddd.repositories import load_tree
+from program_management.ddd.domain.program_tree import ProgramTreeIdentity
+from program_management.ddd.repositories.program_tree import ProgramTreeRepository
 
 
 def education_group_year_pedagogy_edit_post(request, node: Node):
@@ -123,8 +124,8 @@ def education_group_year_pedagogy_edit_get(request, node: Node):
 @can_change_general_information
 def education_group_year_pedagogy_edit(request, education_group_year_id: int):
     offer = EducationGroupYear.objects.get(id=education_group_year_id)
-    standard_version = offer.educationgroupversion_set.get(version_name='', is_transition=False)
-    tree = load_tree.load(standard_version.root_group.element.pk)
+    identity = ProgramTreeIdentity(code=offer.partial_acronym, year=offer.academic_year.year)
+    tree = ProgramTreeRepository.get(identity)
     node = tree.root_node
     if request.method == 'POST':
         return education_group_year_pedagogy_edit_post(request, node)
