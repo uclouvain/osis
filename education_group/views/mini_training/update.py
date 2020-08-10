f76aadc80a6f3df211d3573661d72f2ab6091535
Ndizera Eddy <eddyndizera@gmail.com>
2020-08-10 08:06:18 +0200

    OSIS-4810 aggregate exception clause in one

--- education_group/views/mini_training/update.py
+++ education_group/views/mini_training/update.py
@@ -154,25 +154,8 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
             delete_command = self._convert_form_to_delete_mini_trainings_command(self.mini_training_form)
             return delete_mini_training_with_program_tree_service.delete_mini_training_with_program_tree(delete_command)
 
-        except program_management_exception.ProgramTreeNotEmptyException as e:
-            self.mini_training_form.add_error("end_year", "")
-            self.mini_training_form.add_error(
-                None,
-                _("Imposible to put end date to %(end_year)s: %(msg)s") % {
-                    "msg": e.message,
-                    "end_year": end_year}
-            )
-
-        except exception.TrainingHaveLinkWithEPC as e:
-            self.mini_training_form.add_error("end_year", "")
-            self.mini_training_form.add_error(
-                None,
-                _("Imposible to put end date to %(end_year)s: %(msg)s") % {
-                    "msg": e.message,
-                    "end_year": end_year}
-            )
-
-        except exception.TrainingHaveEnrollments as e:
+        except (program_management_exception.ProgramTreeNotEmptyException, exception.TrainingHaveLinkWithEPC,
+                exception.TrainingHaveEnrollments) as e:
             self.mini_training_form.add_error("end_year", "")
             self.mini_training_form.add_error(
                 None,
