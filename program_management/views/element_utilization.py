cc498526694b7dc1baac7fdef6c08cef1d4c7160
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-09-08 14:41:41 +0200

    OSIS-4985

--- program_management/views/element_utilization.py
+++ program_management/views/element_utilization.py
@@ -23,7 +23,7 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from typing import Dict
+from typing import Dict, List, Any
 
 from django.urls import reverse
 
@@ -47,7 +47,11 @@ class LearningUnitUtilization(PermissionRequiredMixin, LearningUnitGeneric):
         cmd = command.GetProgramTreesVersionFromNodeCommand(code=self.node.code, year=self.node.year)
         program_trees_versions = search_tree_versions_using_node_service.search_tree_versions_using_node(cmd)
 
-        context['utilization_rows'] = []
+        context['utilization_rows'] = self._get_utilization_rows(program_trees_versions)
+        return context
+
+    def _get_utilization_rows(self, program_trees_versions):
+        utilization_rows = []
         parents = []
         for program_tree_version in program_trees_versions:
             tree = program_tree_version.get_tree()
@@ -56,29 +60,33 @@ class LearningUnitUtilization(PermissionRequiredMixin, LearningUnitGeneric):
                 parent_node_identity = NodeIdentity(code=link.parent.code, year=link.parent.year)
                 if parent_node_identity not in parents:
                     parents.append(parent_node_identity)
-                    trainings = self._find_trainings_using_learning_unit_year(link)
-
-                    context['utilization_rows'].append(
-                        {'link': link,
-                         'link_parent_version_label': get_program_tree_version_name(
-                             parent_node_identity,
-                             ProgramTreeVersionRepository.search_all_versions_from_root_node(parent_node_identity)
-                         ),
-                         'root_nodes': trainings,
-                         'root_version_label': "{}".format(
-                             program_tree_version.version_label if program_tree_version.version_label else ''
-                         ),
-                         }
+                    utilization_rows.append(
+                        self._build_utilization_row(link, parent_node_identity, program_tree_version,
+                                                    program_trees_versions)
                     )
-        context['utilization_rows'] = sorted(context['utilization_rows'], key=lambda row: row['link'].parent.code)
-        return context
+        return sorted(utilization_rows, key=lambda row: row['link'].parent.code)
+
+    def _build_utilization_row(self, link, parent_node_identity, program_tree_version, program_trees_versions):
+        return {'link': link,
+                'link_parent_version_label': get_program_tree_version_name(
+                    parent_node_identity,
+                    ProgramTreeVersionRepository.search_all_versions_from_root_node(parent_node_identity)
+                ),
+                'root_nodes': self._find_trainings_using_link(link.parent, program_trees_versions),
+                'root_version_label': "{}".format(
+                    program_tree_version.version_label if program_tree_version.version_label else ''
+                ),
+                }
 
     @staticmethod
-    def _find_trainings_using_learning_unit_year(link: 'Link') -> Dict:
-        cmd = command.GetProgramTreesVersionFromNodeCommand(code=link.parent.code, year=link.parent.year)
-        program_trees_versions_using_lu = search_tree_versions_using_node_service.search_tree_versions_using_node(cmd)
+    def _find_trainings_using_link(link: 'Link', program_trees_versions) -> List[Dict[str, Any]]:
+        program_trees_versions_using_link = []
+        for program_trees_version in program_trees_versions:
+            if program_trees_version.get_tree().get_node_by_code_and_year(link.code, link.year):
+                program_trees_versions_using_link.append(program_trees_version)
+
         trainings = []
-        for program_tree_version in program_trees_versions_using_lu:
+        for program_tree_version in program_trees_versions_using_link:
             root_node = program_tree_version.get_tree().root_node
             trainings.append({
                 'root_node': root_node,
