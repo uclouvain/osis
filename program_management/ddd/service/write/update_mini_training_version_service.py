e763282107a695e28e8f6aabca555d53cde86549
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2020-09-09 11:56:11 +0200

    OSIS-1823-end-year implement service to update mini training version

--- program_management/ddd/service/write/update_mini_training_version_service.py
+++ program_management/ddd/service/write/update_mini_training_version_service.py
@@ -23,12 +23,85 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
+from django.db import transaction
 
-from program_management.ddd.command import UpdateMiniTrainingVersionCommand
+from education_group.ddd.command import UpdateGroupCommand
+from education_group.ddd.service.write import update_group_service
 from program_management.ddd.business_types import *
+from program_management.ddd.command import UpdateProgramTreeVersionCommand, \
+    UpdateMiniTrainingVersionCommand, DeleteSpecificVersionCommand
+from program_management.ddd.domain.service.calculate_end_postponement import CalculateEndPostponement
+from program_management.ddd.domain.service.identity_search import GroupIdentitySearch
+from program_management.ddd.service.write import update_program_tree_version_service, delete_specific_version_service
 
 
+@transaction.atomic
 def update_mini_training_version(
         command: 'UpdateMiniTrainingVersionCommand',
 ) -> 'ProgramTreeVersionIdentity':
-    pass
+
+    tree_version_identity = update_program_tree_version_service.update_program_tree_version(
+        __convert_to_update_tree_version_command(command)
+    )
+
+    update_group_service.update_group(
+        __convert_to_update_group_command(command, tree_version_identity)
+    )
+
+    return tree_version_identity
+
+
+def __convert_to_update_group_command(
+        cmd: 'UpdateMiniTrainingVersionCommand',
+        tree_version_identity: 'ProgramTreeVersionIdentity'
+) -> 'UpdateGroupCommand':
+    group_identity = GroupIdentitySearch().get_from_tree_version_identity(tree_version_identity)
+    return UpdateGroupCommand(
+        code=group_identity.code,
+        year=cmd.year,
+        abbreviated_title=cmd.offer_acronym,
+        title_fr=cmd.title_fr,
+        title_en=cmd.title_en,
+        credits=cmd.credits,
+        constraint_type=cmd.constraint_type,
+        min_constraint=cmd.min_constraint,
+        max_constraint=cmd.max_constraint,
+        management_entity_acronym=cmd.management_entity_acronym,
+        teaching_campus_name=cmd.teaching_campus_name,
+        organization_name=cmd.teaching_campus_organization_name,
+        remark_fr=cmd.remark_fr,
+        remark_en=cmd.remark_en,
+        end_year=cmd.end_year,
+    )
+
+
+def __convert_to_update_tree_version_command(command: 'UpdateMiniTrainingVersionCommand'):
+    return UpdateProgramTreeVersionCommand(
+        end_year=command.end_year,
+        offer_acronym=command.offer_acronym,
+        version_name=command.version_name,
+        year=command.year,
+        is_transition=command.is_transition,
+        title_en=command.title_en,
+        title_fr=command.title_fr,
+    )
+
+
+def __call_delete_service(program_tree_version: 'ProgramTreeVersion', end_year_updated: int):
+    identity = program_tree_version.entity_identity
+
+    postponement_limit = CalculateEndPostponement.calculate_end_postponement_limit()
+
+    end_year = program_tree_version.end_year_of_existence or postponement_limit
+    end_year_updated = end_year_updated or postponement_limit
+
+    if end_year > end_year_updated:
+        for year_to_delete in range(end_year_updated, program_tree_version.end_year_of_existence):
+            delete_specific_version_service.delete_specific_version(
+                DeleteSpecificVersionCommand(
+                    acronym=identity.offer_acronym,
+                    year=year_to_delete,
+                    version_name=identity.version_name,
+                    is_transition=identity.is_transition,
+                )
+            )
