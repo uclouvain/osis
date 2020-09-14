4bf7df6769080130e164c95ab8c28b02892c1f20
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-09-14 14:27:14 +0200

    OSIS-5048

--- base/models/organization.py
+++ base/models/organization.py
@@ -84,7 +84,7 @@ class Organization(SerializableModel):
     logo_tag.short_description = 'Logo'
 
     class Meta:
-        # ordering = (F("is_current_partner").desc(), "name")
+        ordering = (("name"), )
         permissions = (
             ("can_access_organization", "Can access organization"),
         )
