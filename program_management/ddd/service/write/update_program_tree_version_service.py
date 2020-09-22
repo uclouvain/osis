6a9a8a9ff4d196336837d7905c4082e51c64115e
Ndizera Eddy <eddyndizera@gmail.com>
2020-09-22 14:29:30 +0200

    OSIS-4995 fix suppress tree version when end date different from none

--- program_management/ddd/service/write/update_program_tree_version_service.py
+++ program_management/ddd/service/write/update_program_tree_version_service.py
@@ -73,7 +73,7 @@ def __call_delete_service(program_tree_version: 'ProgramTreeVersion', end_year_u
     end_year_updated = end_year_updated or postponement_limit
 
     if end_year > end_year_updated:
-        limit = program_tree_version.end_year_of_existence or postponement_limit + 1
+        limit = (program_tree_version.end_year_of_existence or postponement_limit) + 1
         for year_to_delete in range(end_year_updated + 1, limit):
             delete_specific_version_service.delete_specific_version(
                 DeleteSpecificVersionCommand(
