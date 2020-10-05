beacd2f3ce88a169be00d9c689ca2c3bef6de0a0
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-10-05 11:45:55 +0200

    OSIS-5193 : Périodicité: typo's

--- base/models/enums/learning_unit_year_periodicity.py
+++ base/models/enums/learning_unit_year_periodicity.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,8 +23,7 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django.utils.translation import gettext_lazy as _
-
+from django.utils.translation import gettext_lazy as _, pgettext_lazy
 from base.models.utils.utils import ChoiceEnum
 
 ANNUAL = "ANNUAL"
@@ -34,7 +33,7 @@ BIENNIAL_ODD = "BIENNIAL_ODD"
 
 # FIXME :: DEPRECATED - use PeriodicityEnum instead
 PERIODICITY_TYPES = (
-    (ANNUAL, _("Annual periodicity")),
+    (ANNUAL, pgettext_lazy("female", "Annual"), ),
     (BIENNIAL_EVEN, _("Biennial even")),
     (BIENNIAL_ODD, _("Biennial odd")),
 )
