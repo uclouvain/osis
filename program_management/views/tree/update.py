138f0ee6bc36952a3e7824a47d79c71232af1a90
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-10-23 14:47:34 +0200

    OSIS-5282 : Fix if no change of links

--- program_management/views/tree/update.py
+++ program_management/views/tree/update.py
@@ -29,7 +29,6 @@ from django import shortcuts
 from django.utils.functional import cached_property
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import FormView
-from osis_common.ddd.interface import BusinessException
 
 from base.forms.exceptions import InvalidFormException
 from base.models.enums.link_type import LinkTypes
@@ -37,6 +36,7 @@ from base.views.common import display_success_messages
 from base.views.mixins import AjaxTemplateMixin
 from education_group.models.group_year import GroupYear
 from osis_common.ddd import interface
+from osis_common.ddd.interface import BusinessException
 from osis_role.contrib.views import AjaxPermissionRequiredMixin
 from program_management.ddd import command
 from program_management.ddd.business_types import *
@@ -116,7 +116,8 @@ class UpdateLinkView(AjaxPermissionRequiredMixin, AjaxTemplateMixin, FormView):
     def form_valid(self, form: ContentFormSet):
         try:
             links = form.save()
-            display_success_messages(self.request, self.get_success_message(links[0]))
+            if links:
+                display_success_messages(self.request, self.get_success_message(links[0]))
             return super().form_valid(form)
         except InvalidFormException:
             return self.form_invalid(form)
