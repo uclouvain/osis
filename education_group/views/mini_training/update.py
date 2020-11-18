4ab2bcbb5120063522296964e174e00868c8f986
Laurent Spitaels <laurent.spitaels@uclouvain.be>
2020-11-18 09:35:16 +0100

    OSIS-5252 debug for mini-training int > AcademicYear

--- education_group/views/mini_training/update.py
+++ education_group/views/mini_training/update.py
@@ -133,7 +133,7 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
         return []
 
     def delete_mini_training(self) -> List['MiniTrainingIdentity']:
-        end_year = self.mini_training_form.cleaned_data["end_year"]
+        end_year = self.mini_training_form.cleaned_data["end_year"].year
         if not end_year:
             return []
         try:
@@ -199,7 +199,7 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
             mini_trainings_identities: List['MiniTrainingIdentity']) -> List[str]:
         last_identity = mini_trainings_identities[-1]
         is_new_end_year_lower_than_initial_one = operator.is_year_lower(
-            self.mini_training_form.cleaned_data["end_year"],
+            self.mini_training_form.cleaned_data["end_year"].year,
             self.mini_training_form.initial['end_year']
         )
         if is_new_end_year_lower_than_initial_one:
@@ -207,7 +207,7 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
                 "Mini-Training %(acronym)s successfully deleted from %(academic_year)s."
             ) % {
                 "acronym": last_identity.acronym,
-                "academic_year": display_as_academic_year(self.mini_training_form.cleaned_data["end_year"] + 1)
+                "academic_year": display_as_academic_year(self.mini_training_form.cleaned_data["end_year"].year + 1)
             }
             return [delete_message]
         return []
@@ -273,7 +273,7 @@ class MiniTrainingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, View):
             title_en=cleaned_data['title_en'],
             keywords=cleaned_data['keywords'],
             management_entity_acronym=cleaned_data['management_entity'],
-            end_year=cleaned_data['end_year'],
+            end_year=cleaned_data['end_year'].year,
             teaching_campus_name=cleaned_data['teaching_campus']['name'],
             teaching_campus_organization_name=cleaned_data['teaching_campus']['organization_name'],
             constraint_type=cleaned_data['constraint_type'],
