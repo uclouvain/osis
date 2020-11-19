dd93ecd3ddae044725b4505ee5763afd09e415c7
Ndizera Eddy <eddyndizera@gmail.com>
2020-11-19 08:29:25 +0100

    OSIS-4243 refactor code

--- assessments/templatetags/tutor_deadline.py
+++ assessments/templatetags/tutor_deadline.py
@@ -36,12 +36,8 @@ ScoresNotYetSubmittedCount = int
 
 @register.filter
 def ordered_deadlines_to_display(deadlines: Dict[datetime.date, ScoresNotYetSubmittedCount]):
-    deadlines_to_display = {
-        deadline: scores_not_yet_submitted for deadline, scores_not_yet_submitted in deadlines.items()
-    }
     return OrderedDict(
         sorted(
-            deadlines_to_display.items(),
-            key=lambda key_value: (not bool(key_value[1]), key_value[0])
-        )
+            deadlines.items(),
+            key=lambda key_value: (not bool(key_value[1]), key_value[0]))
     )
