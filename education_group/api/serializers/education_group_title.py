6b443c1b2fc1c3d111d7438730b4fd8055919408
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-11-24 10:43:22 +0100

    OSIS-5376 : Fix some titles

--- education_group/api/serializers/education_group_title.py
+++ education_group/api/serializers/education_group_title.py
@@ -57,9 +57,8 @@ class EducationGroupTitleAllLanguagesSerializer(EducationGroupTitleSerializer):
 
 
 def _get_offer_title_from_lang(version, lang: str):
-    field_suffix = '_en' if lang == settings.LANGUAGE_CODE_EN else '_fr'
-    field_name = 'title' + field_suffix
-    title = getattr(version.root_group, 'title' + field_suffix)
+    field_name = 'title' + ('_en' if lang == settings.LANGUAGE_CODE_EN else '_fr')
+    title = getattr(version.offer, 'title' + ('_english' if lang == settings.LANGUAGE_CODE_EN else ''))
     version_title = getattr(version, field_name)
     title_suffix = ' [{}]'.format(version_title) if version_title else ''
     return title + title_suffix if title else None
