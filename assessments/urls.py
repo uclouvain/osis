2f7cb5ee6fbf311e397c7acf4af60945376c5667
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2020-12-01 14:47:55 +0100

    OSIS-5413 : Faire pour que l'entièreté du mail de soumission de notes soit dans la langue de l'utilisateur (ou dans la langue par défaut de l'application)

--- assessments/urls.py
+++ assessments/urls.py
@@ -32,7 +32,6 @@ from assessments.views.pgm_manager_administration import ProgramManagerListView,
     ProgramManagerPersonDeleteView
 from assessments.views.scores_responsible import ScoresResponsibleSearch
 
-
 urlpatterns = [
     url(r'^scores_encoding/', include([
         url(r'^$', score_encoding.scores_encoding, name='scores_encoding'),
