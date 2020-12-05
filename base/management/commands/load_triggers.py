4ca7a99a8dff6ec8c5ecccf82f1ddb6f56087d91
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2020-12-06 00:50:19 +0100

    OSIS-5403 : Refactor

--- base/management/commands/load_triggers.py
+++ base/management/commands/load_triggers.py
@@ -8,18 +8,44 @@ from django.db import connection
 class Command(BaseCommand):
     triggers_path = 'backoffice/triggers/'
     lock_file = 'lock.sql'
+    lock_string = """"""
     files_to_ignore = [
         lock_file
     ]
+    cursor = None
+
+    def __init__(self):
+        super().__init__()
+        self.lock_string = self.load_lock()
+        self.cursor = connection.cursor()
 
     def handle(self, *args, **kwargs):
         print("##### Starting loading triggers from {} #####".format(self.triggers_path))
-        lock_string = self.load_lock()
-        self.load_triggers(self.triggers_path, lock_string)
+        self.load_triggers()
 
     def load_lock(self):
         lock_path = self.triggers_path + self.lock_file
         return self._get_sql_string_from_file(path=lock_path)
+
+    def load_triggers(self):
+        for trigger_filename in os.listdir(self.triggers_path):
+            if trigger_filename not in self.files_to_ignore:
+                self.load_trigger(trigger_filename)
+        print("##### Loading triggers finished #####")
+
+    def load_trigger(self, trigger_filename):
+        trigger_string = self._get_sql_string_from_file(path=self.triggers_path + trigger_filename)
+        table_name = re.search(r'ON.*(public..*)\n.*FOR', trigger_string, re.IGNORECASE).group(1)
+        sql_script = self.lock_string.format(
+            table_name=table_name,
+            trigger_sql=trigger_string
+        )
+        print("#### Load trigger from {filename} ####".format(filename=trigger_filename))
+        print("#### Lock table {tablename} ####".format(tablename=table_name))
+        self.cursor.execute(sql_script)
+        print("#### Table {tablename} unlocked ####".format(
+            tablename=table_name
+        ))
 
     @staticmethod
     def _get_sql_string_from_file(path: str):
@@ -28,23 +54,3 @@ class Command(BaseCommand):
             print("##### Error: Should load sql file but it is a {} extension".format(file_extension))
         file = open(path)
         return file.read()
-
-    def load_triggers(self, path: str, lock_string: str):
-        cursor = connection.cursor()
-        for trigger_filename in os.listdir(path):
-            if trigger_filename not in self.files_to_ignore:
-                trigger_string = self._get_sql_string_from_file(path=path + trigger_filename)
-                table_name = re.search(r'ON.*(public..*)\n.*FOR', trigger_string, re.IGNORECASE).group(1)
-                sql_script = lock_string.format(
-                    table_name=table_name,
-                    trigger_sql=trigger_string
-                )
-                print("#### Load trigger from {filename} and lock table {tablename} ####".format(
-                    filename=trigger_filename,
-                    tablename=table_name
-                ))
-                cursor.execute(sql_script)
-                print("#### Table {tablename} unlocked ####".format(
-                    tablename=table_name
-                ))
-        print("##### Loading triggers finished #####")
