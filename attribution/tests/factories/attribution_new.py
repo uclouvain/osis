d976130b8912c98352f0abc3e1bf1efe797fcdf2
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2021-01-04 13:10:09 +0100

    OSIS-4220 : Ajout du champ dans la factory

--- attribution/tests/factories/attribution_new.py
+++ attribution/tests/factories/attribution_new.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -50,3 +50,4 @@ class AttributionNewFactory(factory.django.DjangoModelFactory):
     tutor = factory.SubFactory(TutorFactory)
     score_responsible = False
     learning_container_year = factory.SubFactory(LearningContainerYearFactory)
+    decision_making = None
