62dfb33c88025f232cca692a96bdffdd3dde771e
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-01-05 14:33:17 +0100

    OSIS-4142 : fix external form

--- base/forms/learning_unit/external_learning_unit.py
+++ base/forms/learning_unit/external_learning_unit.py
@@ -106,7 +106,7 @@ class LearningUnitYearForExternalModelForm(LearningUnitYearModelForm):
     class Meta(LearningUnitYearModelForm.Meta):
         fields = ('academic_year', 'acronym', 'specific_title', 'specific_title_english', 'credits',
                   'session', 'quadrimester', 'status', 'internship_subtype', 'attribution_procedure',
-                  'professional_integration', 'campus', 'language', 'periodicity')
+                  'professional_integration', 'campus', 'language', 'periodicity', 'other_remark', 'faculty_remark')
 
         widgets = {
             'campus': autocomplete.ModelSelect2(
