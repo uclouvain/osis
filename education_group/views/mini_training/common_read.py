49c4cab3edbc7c24386a88870cc6d15486839de3
Christophe Fivet <christophe.fivet@uclouvain.be>
2021-01-19 14:41:45 +0100

    OSIS-4387 - Empêcher de pouvoir créer une version de transition à partir d'une version de transition

--- education_group/views/mini_training/common_read.py
+++ education_group/views/mini_training/common_read.py
@@ -255,7 +255,7 @@ class MiniTrainingRead(PermissionRequiredMixin, ElementSelectedClipBoardMixin, T
             ) + "?path={}".format(self.get_path())
 
     def get_create_transition_version_url(self):
-        if self.is_root_node():
+        if self.is_root_node() and not self.program_tree_version_identity.is_transition:
             return reverse(
                 'create_education_group_transition_version',
                 kwargs={'year': self.node_identity.year, 'code': self.node_identity.code}
--- education_group/views/training/common_read.py
+++ education_group/views/training/common_read.py
@@ -264,7 +264,7 @@ class TrainingRead(PermissionRequiredMixin, ElementSelectedClipBoardMixin, Templ
             ) + "?path={}".format(self.path)
 
     def get_create_transition_version_url(self):
-        if self.is_root_node:
+        if self.is_root_node and not self.program_tree_version_identity.is_transition:
             return reverse(
                 'create_education_group_transition_version',
                 kwargs={'year': self.node_identity.year, 'code': self.node_identity.code}
